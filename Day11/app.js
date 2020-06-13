2fb759a663be7f8fd3741e9438e037e2cf573b3e
Johnson Dubula <jdubula@iqbusiness.net>
2020-06-13 21:42:51 +0200

    day 11

---
+++ Day11/app.js
@@ -0,0 +1,13 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+let navToggle = document.querySelector("nav-toggle");
+let links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    }
+});
