ce758aecc6f77c2b6c29d98c14c0d7ba5a83a2f6
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-06 05:40:29 +0200

    java

--- java/src/com/company/Main.java
+++ java/src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world");
+        String name = "jam";
+        System.out.println("hello world by " + name);
+
 
     }
 }
