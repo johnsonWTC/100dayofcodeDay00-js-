bfdc49f3d35e3a1c1e3266fb0863f668e19c8c65
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-06 10:24:06 +0200

    java

--- java/src/com/javaday00/Main.java
+++ java/src/com/javaday00/Main.java
@@ -5,6 +5,10 @@ package com.javaday00;
 public class Main {
 
     public static void main(String[] args) {
+
+        Heroes createHerro = new Heroes(4);
+         String newHero = createHerro.herroType();
+        System.out.println(newHero);
     }
 
 
