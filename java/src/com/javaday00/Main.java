95e257171c83e946c59e881fc38a697393a983e2
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-06 10:33:57 +0200

    aditable form

--- java/src/com/javaday00/Main.java
+++ java/src/com/javaday00/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Heroes createHerro = new Heroes(4);
+        Heroes createHerro = new Heroes(14);
          String newHero = createHerro.herroType();
         System.out.println(newHero);
     }
