66873e27a5e71938babb2d641cee528b6a62a1f5
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-06 17:21:37 +0200

    java

--- java/src/com/Day0002/STDBank.java
+++ java/src/com/Day0002/STDBank.java
@@ -1,32 +1,48 @@
 package com.Day0002;
 
+import java.util.ArrayList;
+
 public class STDBank {
     String name;
     private int age;
-    private int bankBalance;
+    private int bankBalance = 0;
+    private ArrayList bankAccounts = new ArrayList();
 
-    public STDBank(String name) {
+    public STDBank(String name,int age) {
         this.name = name;
-    }
-
-    public void setAge(int age) {
         this.age = age;
     }
 
-    public int getAge() {
-        return age;
-    }
+  public  void openAccount(){
+        if(age > 70) {
+            System.out.println("you are too old to open a bank account");
+        }else if (age < 18){
+            System.out.println(" you are too young to open a bank account");
+        }else {
+            if(bankAccounts.contains(name)){
+                System.out.println(" already has an account");
+            }else{
+                bankAccounts.add(name);
+                System.out.println(name +" now has an account");
+            }
 
-    public int getBankBalance() {
-        return bankBalance;
+        }
+  }
+
+    public void checkBalance() {
+        System.out.println("your current balance is "+ this.bankBalance);
     }
 
     public void depositMoney(int funds) {
-        if(bankBalance > 5000){
-            System.out.println("You have too much money in your account, please open an investment account");
+        if (bankAccounts.contains(this.name)) {
+            if (bankBalance > 5000) {
+                System.out.println("You have too much money in your account, please open an investment account");
+            } else {
+                this.bankBalance = funds + this.bankBalance;
+                System.out.println("your new balance is " + this.bankBalance);
+            }
         }else {
-            this.bankBalance = funds + this.bankBalance;
-            System.out.println("your new balance is "+ this.bankBalance);
+            System.out.println(this.name + " does not have an acount with us");
         }
     }
 }
--- java/src/com/Day0002/TheBank.java
+++ java/src/com/Day0002/TheBank.java
@@ -1,4 +1,12 @@
 package com.Day0002;
 
 public class TheBank {
+    public static void main(String[] args) {
+        STDBank johnson = new STDBank("johnson", 30);
+        johnson.checkBalance();
+        johnson.depositMoney(40000000);
+        johnson.openAccount();
+        johnson.depositMoney(40000000);
+
+    }
 }
