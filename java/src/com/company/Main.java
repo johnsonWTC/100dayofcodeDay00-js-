19cd651a881d36613109e1f30cd28084a1d9c15e
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-06 05:51:29 +0200

    java

--- java/src/com/company/Main.java
+++ java/src/com/company/Main.java
@@ -4,8 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "jam";
-        System.out.println("hello world by " + name);
+        int age = 210;
+
+        MyClass test = new MyClass();
+        test.nameEvaluator(age);
+
+
+
 
 
     }
+
+    public static  class MyClass {
+        public void nameEvaluator(int personsAge) {
+
+            if (personsAge > 21) {
+                System.out.println("umdala");
+            } else {
+                System.out.println("no you are ok");
+            }
+        }
+    }
+
 }
