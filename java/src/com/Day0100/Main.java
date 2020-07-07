da086800180922efec0742668937edd5672bca45
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-07 14:07:01 +0200

    java

--- java/src/com/Day0100/Main.java
+++ java/src/com/Day0100/Main.java
@@ -2,4 +2,17 @@ package com.Day0100;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Capitec johnson = new Capitec("johnson", 34,true);
+        johnson.depositFunds(25);
+        johnson.createAccount();
+        johnson.depositFunds(400);
+        johnson.depositFunds(400);
+        johnson.withdrawFunds(200);
+
+
+    }
+
+
+
 }
