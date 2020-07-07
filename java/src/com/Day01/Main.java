93c3f157a596d94d0d4af5907fbd3874e651b832
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-07 10:40:14 +0200

    DOM

--- java/src/com/Day01/Main.java
+++ java/src/com/Day01/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         MyBank johnson = new MyBank("johnson",65);
         johnson.checkBalance();
-        johnson.depositFunds(400);
-        johnson.withDrawFunds(400);
-        johnson.createAccount();
-        johnson.withDrawFunds(700);
-        johnson.depositFunds(1000);
-        johnson.withDrawFunds(300);
+//        johnson.depositFunds(400);
+//        johnson.withDrawFunds(400);
+       johnson.createAccount();
+//        johnson.withDrawFunds(700);
+//        johnson.depositFunds(1000);
+//        johnson.withDrawFunds(300);
         johnson.checkBalance();
+        johnson.depositFunds(300);
+        johnson.checkBalance();
+        johnson.withDrawFunds(100);
+        MyBank malebo = new MyBank("malebo",27);
+        malebo.depositFunds(4000);
+        malebo.createAccount();
 
     }
 }
