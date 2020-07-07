acd2691dc3dca31aa069f448a2088a9cc0405e4f
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-07 03:39:35 +0200

    java

--- java/src/com/Day01/MyBank.java
+++ java/src/com/Day01/MyBank.java
@@ -2,4 +2,11 @@ package com.Day01;
 
 public class MyBank {
 
+    String name;
+    int age;
+
+    public MyBank(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
