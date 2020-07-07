b0c7e0c6eef72d577f486511ba50ec8186d4da42
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-07 03:49:48 +0200

    java

--- java/src/com/Day01/MyBank.java
+++ java/src/com/Day01/MyBank.java
@@ -4,9 +4,22 @@ public class MyBank {
 
     String name;
     int age;
+    int bankBalance = 0;
+    String accountType;
+
 
     public MyBank(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public void checkBalance(){
+        System.out.println(bankBalance + " Your current balance");
+    }
+
+    public void depositFunds(int funds){
+        if(funds > 10000){
+            System.out.println("This amount is too large to deposit");
+        }
     }
 }
