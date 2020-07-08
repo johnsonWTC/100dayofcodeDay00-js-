dfc8d2e11f6f61c2e5766f3760e75cc9fd0b5ea6
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-08 14:24:08 +0200

    java

---
+++ java/src/com/Day02/Capitec.java
@@ -0,0 +1,19 @@
+package com.Day02;
+
+public class Capitec implements IBank {
+
+    @Override
+    public void createAccount() {
+
+    }
+
+    @Override
+    public void depositFunds() {
+
+    }
+
+    @Override
+    public void withdrawFund() {
+
+    }
+}
---
+++ java/src/com/Day02/IBank.java
@@ -0,0 +1,11 @@
+package com.Day02;
+
+public interface IBank {
+    int interest = 10;
+    void createAccount();
+    void depositFunds();
+    void withdrawFund();
+    public static void getBalance(){
+
+    }
+}
