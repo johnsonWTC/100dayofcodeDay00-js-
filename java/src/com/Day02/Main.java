56f26ff897a8c5354da924e2985977b84a822c76
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-08 15:40:25 +0200

    java

--- java/src/com/Day02/Main.java
+++ java/src/com/Day02/Main.java
@@ -1,9 +1,26 @@
 package com.Day02;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        IBank Johnson = new Capitec(36,"Johnson");
-        Johnson.depositFunds(30);
+        boolean game = true;
+        while(game){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello, welcome to our ATM");
+            System.out.println("*************************");
+            System.out.println("Do you have an account with us?\n"
+                    +"1.Yes\n"
+                            +"2.No\n");
+            int i  = scanner.nextInt();
+            System.out.println("What would you like to do?\n"+
+            "1.Withdraw Money\n"
+            +"2.Deposit funds\n"
+            +"3.Create account");
+            String name = scanner.nextLine();
+            System.out.println("your name is " + name);
+            break;
+        }
     }
 }
