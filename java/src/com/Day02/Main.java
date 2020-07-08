ad885e595cd8351f825fda534001d533a1ffab5c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-08 15:55:45 +0200

    java

--- java/src/com/Day02/Main.java
+++ java/src/com/Day02/Main.java
@@ -6,21 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         boolean game = true;
-        while(game){
+        while (game) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Hello, welcome to our ATM");
             System.out.println("*************************");
             System.out.println("Do you have an account with us?\n"
-                    +"1.Yes\n"
-                            +"2.No\n");
-            int i  = scanner.nextInt();
-            System.out.println("What would you like to do?\n"+
-            "1.Withdraw Money\n"
-            +"2.Deposit funds\n"
-            +"3.Create account");
-            String name = scanner.nextLine();
-            System.out.println("your name is " + name);
-            break;
+                    + "1.Yes\n"
+                    + "2.No\n");
+            Integer i = scanner.nextInt();
+            if (i.equals(2)) {
+                System.out.println("******create account******\n"
+                        + "what is you name");
+                String name = scanner.nextLine();
+                System.out.println("what is your age");
+                Integer age = scanner.nextInt();
+                IBank client = new Capitec(age, name);
+                client.createAccount();
+                System.out.println("What would you like to do?\n" +
+                        "1.Deposit Money\n"
+                        + "2.withdraw funds\n");
+                    i = scanner.nextInt();
+                    if(i.equals(1)){
+                        System.out.println("How much would you like to deposit");
+                        int funds = scanner.nextInt();
+                        client.depositFunds(funds);
+                    }
+                break;
+            }
         }
     }
 }
