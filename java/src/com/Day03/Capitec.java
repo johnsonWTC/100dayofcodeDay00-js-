5b854547543c4d11232d035b99a8bd744999f7b2
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-09 11:00:12 +0200

    java

--- java/src/com/Day03/Capitec.java
+++ java/src/com/Day03/Capitec.java
@@ -1,21 +1,40 @@
 package com.Day03;
 
+import java.util.ArrayList;
+
 public class Capitec implements IBank {
 
+    int age;
+    String name;
+    int accountBalance;
+    ArrayList<String> Accounts = new ArrayList();
 
     @Override
     public void createAccount(String name, int age) {
-
+        if(Accounts.contains(this.name)){
+            System.out.println(this.name+ " Already has an account");
+        }else if(this.age > 45){
+            System.out.println("You are too old this");
+        }else{
+            Accounts.add(this.name);
+            System.out.println("Account has been created for "+ this.name);
+        }
     }
 
     @Override
     public void checkBalance() {
-
+        if(!Accounts.contains(this.name)){
+            System.out.println(this.name+ " doesnt have an account with us");
+        }else{
+            System.out.println("Hello "+this.name+" your balance is "+ accountBalance);
+        }
     }
 
     @Override
     public void depositFunds(int funds) {
-
+        if(!Accounts.contains(this.name)){
+            System.out.println(this.name+ " doesnt have an account with us");
+        }
     }
 
     @Override
