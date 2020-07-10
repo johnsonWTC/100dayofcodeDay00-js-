9873b75f4fdf9ea7c4e012a05ac7248626548cf2
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-10 19:38:11 +0200

    java

--- java/src/com/Day04/Main.java
+++ java/src/com/Day04/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         IBank fnb = new FNB("johnson", 30);
+        fnb.createAccount();
         fnb.depositFunds(3000);
+        fnb.withDrawFunds(400);
         fnb.createAccount();
     }
 }
