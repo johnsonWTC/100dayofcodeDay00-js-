401cff26281951555d07328755352b0abf60d6d8
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-10 19:23:05 +0200

    java

---
+++ java/src/com/Day04/FNB.java
@@ -0,0 +1,18 @@
+package com.Day04;
+
+public class FNB implements IBank {
+    @Override
+    public void createAccount(String name, int age) {
+
+    }
+
+    @Override
+    public void depositFunds(int funds) {
+
+    }
+
+    @Override
+    public void withDrawFunds(int funds) {
+
+    }
+}
