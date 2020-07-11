d8405c91f8a182cdaa8c6abde52aef86fa4d9020
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-11 03:01:53 +0200

    java

--- java/src/com/Day05/BankSetter.java
+++ java/src/com/Day05/BankSetter.java
@@ -5,27 +5,46 @@ import java.util.ArrayList;
 public class BankSetter {
     String name;
     int age;
+    int bankBalance = 0;
     ArrayList<String> bankAccounts = new ArrayList<>();
 
-    public void createdAccount(){
-        if(bankAccounts.contains(this.name)){
+    public void createdAccount() {
+        if (bankAccounts.contains(this.name)) {
             System.out.println(this.name + " already has an account with us");
-        }
-        else if(this.age > 40){
+        } else if (this.age > 40) {
             System.out.println("dude you are too old for this");
-        }else{
+        } else {
             bankAccounts.add(this.name);
-            System.out.println("Account created for "+ this.name);
+            System.out.println("Account created for " + this.name);
         }
 
     }
-    void depositFunds(int funds){
 
+    void depositFunds(int funds) {
+        if (!bankAccounts.contains(this.name)) {
+            System.out.println(this.name + " doesnt have an account with us");
+        } else if (funds > 10000) {
+            System.out.println("Your account cannot hold this much money");
+        } else {
+            bankBalance = bankBalance + funds;
+            System.out.println(this.bankBalance + " was deposited, your new balance is " + this.bankBalance);
+        }
     }
-    void withDrawFunds(int funds){
 
+    void withDrawFunds(int funds) {
+        if (!bankAccounts.contains(this.name)) {
+            System.out.println(this.name + " doesnt have an account with us");
+        } else if (funds > bankBalance) {
+            System.out.println("Insufficient funds");
+        } else {
+            bankBalance = bankBalance - funds;
+            System.out.println(this.bankBalance + " was withdrawn, your new balance is " + this.bankBalance);
+        }
     }
-    void checkBalance(){
-
+       void checkBalances(){
+            if (!bankAccounts.contains(this.name)) {
+                System.out.println(this.name + " doesnt have an account with us");
+            }
+        }
     }
 }
