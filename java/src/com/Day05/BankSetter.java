3a0ad43b62de2d7d29900c0928a37dc8786043c8
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-11 02:37:26 +0200

    java

---
+++ java/src/com/Day05/BankSetter.java
@@ -0,0 +1,17 @@
+package com.Day05;
+
+public class BankSetter {
+
+    public void createdAccount(){
+
+    }
+    void depositFunds(int funds){
+
+    }
+    void withDrawFunds(int funds){
+
+    }
+    void checkBalance(){
+
+    }
+}
