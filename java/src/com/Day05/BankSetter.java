25672ba6d4c0d3801fc1c8bdc184060bf409c36d
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-11 02:48:12 +0200

    java

--- java/src/com/Day05/BankSetter.java
+++ java/src/com/Day05/BankSetter.java
@@ -1,8 +1,22 @@
 package com.Day05;
 
+import java.util.ArrayList;
+
 public class BankSetter {
+    String name;
+    int age;
+    ArrayList<String> bankAccounts = new ArrayList<>();
 
     public void createdAccount(){
+        if(bankAccounts.contains(this.name)){
+            System.out.println(this.name + " already has an account with us");
+        }
+        else if(this.age > 40){
+            System.out.println("dude you are too old for this");
+        }else{
+            bankAccounts.add(this.name);
+            System.out.println("Account created for "+ this.name);
+        }
 
     }
     void depositFunds(int funds){
