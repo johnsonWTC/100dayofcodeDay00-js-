121b98ed6eacdc83da09feab6cff3ffcf2791598
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-12 14:57:32 +0200

    day 06

--- java/src/com/Day06/ReserveBank.java
+++ java/src/com/Day06/ReserveBank.java
@@ -1,6 +1,21 @@
 package com.Day06;
 
+import java.util.ArrayList;
+
 public class ReserveBank {
+    String name;
+    int age;
+    ArrayList<String> names = new ArrayList<>();
 
 
+    public ReserveBank(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void createAccount(){
+        if(names.contains(this.name)){
+            System.out.println(this.name + " Already has an account with us");
+        }
+    }
 }
