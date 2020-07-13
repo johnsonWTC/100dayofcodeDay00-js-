d3c1ba6c8b348f1b696270299f3918016e297e9a
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-13 16:01:31 +0200

    java

--- java/src/com/Day07/Capitec.java
+++ java/src/com/Day07/Capitec.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 public class Capitec implements ReserveBank {
     String name;
     int age;
+    int balance = 0;
+
+    public Capitec(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     ArrayList<String> names = new ArrayList<>();
     @Override
     public void createAccount(String Name, int age) {
@@ -18,16 +25,38 @@ public class Capitec implements ReserveBank {
 
     @Override
     public void checkBalance() {
-
+        if(!names.contains(this.name)){
+            System.out.println(this.name +" does not have an account with us");
+        }else if(age > 30){
+            System.out.println("You are too old for a student account");
+        }
+        else{
+            System.out.println("Hello " +this.name + " your new balance is "+ this.balance);
+        }
     }
 
     @Override
     public void depositFunds(int funds) {
-
+        if(!names.contains(this.name)){
+            System.out.println(this.name +" does not have an account with us");
+        }else if(funds > 10000){
+            System.out.println("The maximum deposit limit 10000 for a student account");
+        }
+        else{
+            System.out.println(funds + " was deposited into your account, your new balance is " +this.balance);
+        }
     }
 
     @Override
-    public void withDrawFunds() {
+    public void withDrawFunds(int funds) {
+        if(!names.contains(this.name)){
+            System.out.println(this.name +" does not have an account with us");
+        }else if(funds > 10000){
+            System.out.println("The maximum deposit limit 10000 for a student account");
+        }
+        else{
+            System.out.println(funds + " was deposited into your account, your new balance is " +this.balance);
+        }
 
     }
 }
--- java/src/com/Day07/ReserveBank.java
+++ java/src/com/Day07/ReserveBank.java
@@ -5,5 +5,5 @@ public interface ReserveBank {
     void createAccount(String Name, int age);
     void checkBalance();
     void depositFunds(int funds);
-    void withDrawFunds();
+    void withDrawFunds (int funds);
 }
