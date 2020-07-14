9a58d2d47286f076fc6705cb9334dcb565ec25d2
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-14 16:48:01 +0200

    java

--- dayten/src/main/java/day10/dayten/Hello.java
+++ dayten/src/main/java/day10/dayten/Hello.java
@@ -1,4 +1,13 @@
 package day10.dayten;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Hello {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello.jsp";
+    }
 }
--- dayten/src/main/webapp/hello.jsp
+++ dayten/src/main/webapp/hello.jsp
@@ -1,3 +1,7 @@
 <html>
-<h1>Hello</h1>
+<h1>Hello this is Johnson</h1>
+
+    <input placeholder="input type" type="text" id="input">
+<button id="add">Add</button>
+
 </html>
