35c4dc1674af620ce95314474bdce575de0b6756
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-14 08:45:13 +0200

    java

---
+++ SpringMVC01/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class MainController {
+    @ResponseBody
+    @GetMapping("test")
+    public String hello(){
+        return "hello, we are testing";
+    }
+
+}
