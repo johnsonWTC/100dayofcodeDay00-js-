130eaf7883fe55122621ce71939f811a81b005cc
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-15 13:16:23 +0200

    welcome

--- day11java/src/main/java/com/example/day11/Index.java
+++ day11java/src/main/java/com/example/day11/Index.java
@@ -1,12 +1,24 @@
 package com.example.day11;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Index {
 
     public String welcome(){
         return "index.html";
+    }
+    @RequestMapping
+    public ModelAndView welcome(@RequestParam(value = "name", defaultValue = "yourName") String name){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject(name,name);
+        modelAndView.setViewName("welcome.html");
+        return modelAndView;
+
     }
 
 }
