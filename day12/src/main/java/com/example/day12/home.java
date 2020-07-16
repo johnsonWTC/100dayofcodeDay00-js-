b68d6cca1cca46d294094e74dd4330fc1fd020ce
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-16 13:42:40 +0200

    java

---
+++ day12/src/main/java/com/example/day12/home.java
@@ -0,0 +1,18 @@
+package com.example.day12;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class home {
+    @RequestMapping("home")
+    public ModelAndView home(@RequestParam(value = "name", defaultValue = "User") String name){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name",name);
+        modelAndView.setViewName("wel.jsp");
+        return modelAndView;
+    }
+}
