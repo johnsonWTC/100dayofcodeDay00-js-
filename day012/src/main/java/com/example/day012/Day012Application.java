07ff7c6924dfe5a8cbbdfa792999cc42e7506574
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-16 14:21:46 +0200

    java

---
+++ day012/src/main/java/com/example/day012/Day012Application.java
@@ -0,0 +1,13 @@
+package com.example.day012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day012Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day012Application.class, args);
+    }
+
+}
