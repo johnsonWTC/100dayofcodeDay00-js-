1e70e335149249117288bf9124b806cdc48e5d9f
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-16 14:29:15 +0200

    java

--- day012/src/main/java/com/example/day012/welcome.java
+++ day012/src/main/java/com/example/day012/welcome.java
@@ -1,4 +1,12 @@
 package com.example.day012;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class welcome {
+    @GetMapping("welcome")
+    public String welcome(){
+        return "wel";
+    }
 }
