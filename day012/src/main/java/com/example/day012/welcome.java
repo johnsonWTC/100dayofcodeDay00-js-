db573ac1b1207c48bdf3d4be7d7c2e40acf2981d
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-16 16:05:19 +0200

    js

--- day012/src/main/java/com/example/day012/welcome.java
+++ day012/src/main/java/com/example/day012/welcome.java
@@ -2,11 +2,17 @@ package com.example.day012;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class welcome {
-    @GetMapping("welcome")
-    public String welcome(){
-        return "wel";
+    @RequestMapping("welcome")
+    public ModelAndView welcome(@RequestParam(value = "name",defaultValue = "item name") String name){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", name);
+        modelAndView.setViewName("wel.jsp");
+        return modelAndView;
     }
 }
---
+++ day012/src/main/webapp/wel.jsp
@@ -0,0 +1,1 @@
+<h1>Well well well ${name}</h1>
