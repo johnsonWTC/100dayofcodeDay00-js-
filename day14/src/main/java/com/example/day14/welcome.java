4e3d77e90c1a542d1069c4e8b9ab8a2ca3ea63e5
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-17 01:34:30 +0200

    java

--- day14/src/main/java/com/example/day14/welcome.java
+++ day14/src/main/java/com/example/day14/welcome.java
@@ -2,13 +2,17 @@ package com.example.day14;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class welcome {
     @RequestMapping("welcome")
-    public ModelAndView welcome(){
+    public ModelAndView welcome(@RequestParam(value = "item", defaultValue = "Please insert name") String item){
+
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", item);
+        modelAndView.setViewName("wel.jsp");
         return modelAndView;
     }
 }
