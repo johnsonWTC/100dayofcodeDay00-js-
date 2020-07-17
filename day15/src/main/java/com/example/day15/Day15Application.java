9805b03f0e31c81d8f3828a7898c0f770299d49c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-17 11:03:08 +0200

    java

--- day15/src/main/java/com/example/day15/Day15Application.java
+++ day15/src/main/java/com/example/day15/Day15Application.java
@@ -7,4 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day15Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Day15Application.class, args
+        SpringApplication.run(Day15Application.class, args);
+    }
+
+}
---
+++ day15/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<h1>Hello</h1>
+<form action="profile" method="post">
+    <input type="text" name="item" id="item">
+    <button type="submit">Add name</button>
+</form>
