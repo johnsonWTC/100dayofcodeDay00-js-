f14fb63f121d206cd50c0a5ee91c4f2988ac84da
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-18 13:56:22 +0200

    jam

--- day16/src/main/java/com/example/day16/profile.java
+++ day16/src/main/java/com/example/day16/profile.java
@@ -1,7 +1,18 @@
 package com.example.day16;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class profile {
+
+    @RequestMapping("profile")
+    public ModelAndView proflie(@RequestParam(value = "item", defaultValue = "what is your name vele") String item){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name",item);
+        modelAndView.setViewName("profile.jsp");
+        return modelAndView;
+    }
 }
