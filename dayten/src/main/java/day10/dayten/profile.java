cd0388984599da7041217b26be56cf23ffa72181
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-18 21:34:31 +0200

    java

--- dayten/src/main/java/day10/dayten/profile.java
+++ dayten/src/main/java/day10/dayten/profile.java
@@ -2,15 +2,16 @@ package day10.dayten;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class profile {
-    @GetMapping("profile")
+    @RequestMapping("profile")
     public ModelAndView profile(@RequestParam(value = "names",defaultValue = "please insert name") String amagama){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("agama",amagama);
+        modelAndView.addObject("igama",amagama);
         modelAndView.setViewName("profile");
         return modelAndView;
     }
