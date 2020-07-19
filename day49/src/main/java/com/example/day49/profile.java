7eb5c28b4aee1929ef53a0129be8be7972fa6bf8
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-19 06:53:03 +0200

    js

--- day49/src/main/java/com/example/day49/profile.java
+++ day49/src/main/java/com/example/day49/profile.java
@@ -1,4 +1,17 @@
 package com.example.day49;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class profile {
+    @RequestMapping("profile")
+    public ModelAndView profile(@RequestParam(value = "userName",defaultValue = "what is your name") String userName){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name",userName);
+        modelAndView.setViewName("profile.jsp");
+        return modelAndView;
+    }
 }
--- day49/src/main/webapp/profile.jsp
+++ day49/src/main/webapp/profile.jsp
@@ -1,1 +1,3 @@
 <h1>Hello ${name}</h1>
+<input placeholder="item name" id="itemName">
+<button id="add"></button>
