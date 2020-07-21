7a0a101ba210989bedad62ba24c87f7d0a04b2aa
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-21 13:07:15 +0200

    js

---
+++ day050/src/test/java/com/example/day050/Day050ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day050;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day050ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
