6db35b7a468a010dd605a259b25013ecb1489a3e
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-21 19:26:44 +0200

    js

--- day503/src/main/java/com/example/day503/Shop.java
+++ day503/src/main/java/com/example/day503/Shop.java
@@ -2,12 +2,13 @@ package com.example.day503;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Shop {
-
+@RequestMapping("shop")
     public ModelAndView shop(@RequestParam(value = "itemName", defaultValue = "the item name goes here") String item){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("shopItem",item);
