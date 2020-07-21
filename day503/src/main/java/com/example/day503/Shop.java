90a60f74bd18ead5644fe01d56af86ca3610d674
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-21 19:19:57 +0200

    js

--- day503/src/main/java/com/example/day503/Shop.java
+++ day503/src/main/java/com/example/day503/Shop.java
@@ -1,4 +1,15 @@
 package com.example.day503;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class Shop {
+
+    public ModelAndView shop(){
+        ModelAndView modelAndView = new ModelAndView();
+
+        return  modelAndView;
+    }
 }
