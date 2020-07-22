17dc9defcce26589e50c836b2c85e41d8a5753ed
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-22 17:43:36 +0200

    j

--- restful/src/main/java/com/example/restful/Ticket.java
+++ restful/src/main/java/com/example/restful/Ticket.java
@@ -1,4 +1,12 @@
 package com.example.restful;
 
+import javax.persistence.*;
+
+@Entity
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ticketId;
+
+    private String passengerName;
 }
