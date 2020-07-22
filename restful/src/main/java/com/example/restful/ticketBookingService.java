ef4f75eb8540d9f949306bb6861d6ef714336b09
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-22 17:53:37 +0200

    j

--- restful/src/main/java/com/example/restful/ticketBookingService.java
+++ restful/src/main/java/com/example/restful/ticketBookingService.java
@@ -1,7 +1,14 @@
 package com.example.restful;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ticketBookingService {
+    @Autowired
+    private  TicketBookingDoa ticketBookingDoa;
+    public Ticket createTicket(Ticket ticket){
+        return ticketBookingDoa.save(ticket);
+    }
+
 }
