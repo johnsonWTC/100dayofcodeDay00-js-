a9dfb3af356c82154fc547400308477cc51c53d6
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-24 16:22:34 +0200

    java

---
+++ crud52/src/main/java/com/example/crud52/Crud52Application.java
@@ -0,0 +1,13 @@
+package com.example.crud52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud52Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crud52Application.class, args);
+    }
+
+}
