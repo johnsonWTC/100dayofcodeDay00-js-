82a42baef8ad0439f44ba059a8f06701e86d4a7c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 00:25:42 +0200

    day 53

---
+++ restday54/src/main/java/com/example/restday54/Restday54Application.java
@@ -0,0 +1,13 @@
+package com.example.restday54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restday54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restday54Application.class, args);
+    }
+
+}
