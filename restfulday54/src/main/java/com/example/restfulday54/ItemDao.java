4bfa3b57b1135b32f43b6d9de72464351b2a20fc
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 00:49:25 +0200

    java

--- restfulday54/src/main/java/com/example/restfulday54/ItemDao.java
+++ restfulday54/src/main/java/com/example/restfulday54/ItemDao.java
@@ -1,6 +1,7 @@
 package com.example.restfulday54;
 
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemDao extends CrudRepository {
+public interface ItemDao extends CrudRepository <Item,Integer> {
 }
--- restfulday54/src/main/java/com/example/restfulday54/ItemService.java
+++ restfulday54/src/main/java/com/example/restfulday54/ItemService.java
@@ -1,7 +1,28 @@
 package com.example.restfulday54;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+    @Autowired
+    ItemDao itemDao;
+
+    public Item createItem(Item item){
+        return itemDao.save(item);
+    }
+
+    public void deleteItem(Item item){
+        itemDao.delete(item);
+    }
+
+    public Item updateItem(Integer itemId, String itemName){
+        Item item = itemDao.findById(itemId).orElse(null);
+        item.setItemName(itemName);
+        return itemDao.save(item);
+    }
+
+    public Item findbyId(Integer itemId){
+        return itemDao.findById(itemId).orElse(null);
+    }
 }
