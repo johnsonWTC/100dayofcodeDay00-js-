f393e9701a08bf4821a76b54bbb321431da684d9
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 00:28:25 +0200

    java

---
+++ restfulday54/src/main/java/com/example/restfulday54/Restfulday54Application.java
@@ -0,0 +1,13 @@
+package com.example.restfulday54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restfulday54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restfulday54Application.class, args);
+    }
+
+}
---
+++ restfulday54/src/test/java/com/example/restfulday54/Restfulday54ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulday54;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restfulday54ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
