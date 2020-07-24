ab7067d3e5e730275630d00567ac3fb845569d21
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-24 16:39:15 +0200

    js

--- crud52/src/main/java/com/example/crud52/ItemService.java
+++ crud52/src/main/java/com/example/crud52/ItemService.java
@@ -1,7 +1,25 @@
 package com.example.crud52;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    ItemDao itemDao;
+
+    public Item geItemByID(Integer itemID){
+        return itemDao.findById(itemID).orElse(null);
+    }
+
+    public void deleteItemByID(Integer itemID){
+        itemDao.deleteById(itemID);
+    }
+
+    public Item saveItem(Item item){
+       return itemDao.save(item);
+    }
+
+
 }
--- restful/src/main/java/com/example/restful/ticketBookingService.java
+++ restful/src/main/java/com/example/restful/ticketBookingService.java
@@ -13,6 +13,7 @@ public class ticketBookingService {
     }
 
     public Item getItemById(Integer itemId){
+
         return ticketBookingDoa.findById(itemId).orElse(null);
     }
     public void deleteItemById(Integer itemId){
