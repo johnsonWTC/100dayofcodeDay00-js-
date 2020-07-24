e41cb9f2e4861259ca5fd1a56d4605b652df08dd
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-24 16:53:23 +0200

    js

--- crud52/src/main/java/com/example/crud52/ItamController.java
+++ crud52/src/main/java/com/example/crud52/ItamController.java
@@ -1,4 +1,25 @@
 package com.example.crud52;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ItamController {
+
+    @Autowired
+    ItemService itemService;
+
+    @PostMapping("create")
+    public Item createItem (Item item){
+        return itemService.saveItem(item);
+    }
+
+    @GetMapping("{itemId}")
+    public Item getItem(@PathVariable("itemId")  Integer itemId){
+        return itemService.geItemByID(itemId);
+    }
+
 }
