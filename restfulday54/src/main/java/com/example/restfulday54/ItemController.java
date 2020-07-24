d8d6ab2e5df86c15a990d39b56f723baac0f354e
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 00:59:36 +0200

    java

--- restfulday54/src/main/java/com/example/restfulday54/ItemController.java
+++ restfulday54/src/main/java/com/example/restfulday54/ItemController.java
@@ -4,6 +4,7 @@ package com.example.restfulday54;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,13 @@ public class ItemController {
     @Autowired
     ItemService itemService;
 
-    @GetMapping("{newItemId}")
-    public Item findItemById(@PathVariable("newItemId") Integer newItemId){
-        return itemService.findbyId(newItemId);
+    @GetMapping("{itemId}")
+    public Item findItemById(@PathVariable("itemId") Integer itemId){
+        return itemService.findbyId(itemId);
+    }
+
+    @PutMapping("{itemID}/{newItemName}")
+    public Item updateItemById(@PathVariable("itemID") Integer itemID, @PathVariable("newItemName") String newItemName){
+
     }
 }
