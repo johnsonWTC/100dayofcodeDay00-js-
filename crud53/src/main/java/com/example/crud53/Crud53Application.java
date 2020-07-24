d857a658223184d87bdbdeb310d086f14fb8286b
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-24 17:27:41 +0200

    day 53

---
+++ crud53/src/main/java/com/example/crud53/Crud53Application.java
@@ -0,0 +1,13 @@
+package com.example.crud53;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud53Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crud53Application.class, args);
+    }
+
+}
---
+++ crud53/src/test/java/com/example/crud53/Crud53ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud53;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud53ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
