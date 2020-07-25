5230ce214f4b76d1016b85731506b395571a8b0d
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 08:30:56 +0200

    js

--- crudday54/src/main/java/com/example/crudday54/Item.java
+++ crudday54/src/main/java/com/example/crudday54/Item.java
@@ -8,6 +8,22 @@ import javax.persistence.Id;
 @Entity
 public class Item {
 
+    public Integer getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(Integer itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer itemID;
