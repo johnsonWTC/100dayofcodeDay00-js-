9a7a5c60f19b8a7427110605327fcb67a549ebe6
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 15:21:48 +0200

    java

--- crudwithui/src/main/java/com/example/crudwithui/Item.java
+++
@@ -1,4 +0,0 @@
-package com.example.crudwithui;
-
-public class Item {
-}
---
+++ crudwithui/src/main/java/com/example/crudwithui/User.java
@@ -0,0 +1,11 @@
+package com.example.crudwithui;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+    Integer userID;
+    String userName;
+}
