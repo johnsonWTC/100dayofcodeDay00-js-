4cd3e930dd596f47996f44e621f517ee9efd01b4
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 15:22:37 +0200

    java

--- crudwithui/src/main/java/com/example/crudwithui/User.java
+++ crudwithui/src/main/java/com/example/crudwithui/User.java
@@ -2,10 +2,15 @@ package com.example.crudwithui;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer userID;
     String userName;
 }
