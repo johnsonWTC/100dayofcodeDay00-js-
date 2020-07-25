20d68040c9600a55614945a000bda196068b6dd7
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-26 01:21:22 +0200

    js

---
+++ demoday55/src/main/java/com/example/demo/User.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer userID;
+
+    String Username;
+}
