ea30304175a74838b9542ec681ae3dd785249f8c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 15:23:27 +0200

    java

--- crudwithui/src/main/java/com/example/crudwithui/UserDao.java
+++ crudwithui/src/main/java/com/example/crudwithui/UserDao.java
@@ -2,5 +2,5 @@ package com.example.crudwithui;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemDao extends CrudRepository<> {
+public interface UserDao extends CrudRepository<User,Integer> {
 }
