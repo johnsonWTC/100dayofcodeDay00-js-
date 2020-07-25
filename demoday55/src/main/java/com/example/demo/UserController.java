cd53b3809272c0123edf8ec7b2ec82ae925cd23c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-26 01:38:51 +0200

    js

--- demoday55/src/main/java/com/example/demo/UserController.java
+++ demoday55/src/main/java/com/example/demo/UserController.java
@@ -1,4 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("add")
+    public User addUser(User user){
+        return userService.addUser(user);
+    }
 }
