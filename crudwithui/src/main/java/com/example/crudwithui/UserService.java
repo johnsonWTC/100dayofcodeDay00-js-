fa477834c41af78402cd2ca26155387e38dbdcbe
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 15:28:10 +0200

    java

--- crudwithui/src/main/java/com/example/crudwithui/UserService.java
+++ crudwithui/src/main/java/com/example/crudwithui/UserService.java
@@ -1,4 +1,21 @@
 package com.example.crudwithui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    public User createUser(User user){
+        return userDao.save(user);
+    }
+
+    public  User findUser(Integer userId){
+        return userDao.findById(userId).orElse(null);
+    }
+
+
 }
