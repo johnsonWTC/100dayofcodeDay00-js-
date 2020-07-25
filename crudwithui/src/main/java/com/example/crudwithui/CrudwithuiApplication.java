4e45ec26272585bf406058187aabd12e032342de
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 15:13:32 +0200

    java

---
+++ crudwithui/src/main/java/com/example/crudwithui/CrudwithuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudwithui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudwithuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudwithuiApplication.class, args);
+    }
+
+}
