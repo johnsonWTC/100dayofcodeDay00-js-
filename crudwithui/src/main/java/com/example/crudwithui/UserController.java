ba1985b0cdd29be90274a98477800d75cd016d9c
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-25 19:32:54 +0200

    java

--- crudwithui/src/main/java/com/example/crudwithui/UserController.java
+++ crudwithui/src/main/java/com/example/crudwithui/UserController.java
@@ -1,14 +1,26 @@
 package com.example.crudwithui;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-public class UserController {
 
-    @GetMapping("index")
+@RestController
+@RequestMapping("home")
+public class UserController {
+    UserService userService;
+
+
     public String UserHomePage(){
         return ("index.html");
+    }
+
+    @PostMapping("/create")
+    public User createUser(User user){
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable("userName") Integer userId){
+        return userService.findUser(userId);
     }
 }
