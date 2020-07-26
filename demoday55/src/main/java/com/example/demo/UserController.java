8aa52722f9b61b4bb982fb0dc66ccfa2affa06ab
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-26 18:36:44 +0200

    js

--- demoday55/src/main/java/com/example/demo/UserController.java
+++ demoday55/src/main/java/com/example/demo/UserController.java
@@ -7,16 +7,4 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 public class UserController {
 
-    @Autowired
-    UserService userService;
-
-    @PostMapping("add")
-    public User addUser(User user){
-        return userService.addUser(user);
-    }
-
-    @GetMapping("{userID}")
-    public  User getUserById(@PathVariable("userID") Integer userID){
-        return userService.getUserById(userID);
-    }
 }
