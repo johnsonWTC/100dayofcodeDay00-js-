16601d96de061490997d96f6616d62caf9139657
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-26 20:26:24 +0200

    js

--- restful/src/main/java/com/example/restful/ItemAddingController.java
+++ restful/src/main/java/com/example/restful/ItemAddingController.java
@@ -12,23 +12,23 @@ public class ItemAddingController {
     @Autowired
     private ticketBookingService ticketBookingService;
 
-    @PostMapping("/create")
+    @PostMapping("create")
     public Item createItem(Item item){
 
         return ticketBookingService.createItem(item);
     }
 
-    @GetMapping("/{itemId}")
+    @GetMapping("{itemId}")
     public Item getItemById(@PathVariable("itemId") Integer itemId){
         return ticketBookingService.getItemById(itemId);
     }
 
-    @DeleteMapping("/{itemId}")
+    @DeleteMapping("{itemId}")
         public void deleteItemById(@PathVariable("itemId") Integer itemId){
             ticketBookingService.deleteItemById(itemId);
         }
 
-    @GetMapping("/all")
+    @GetMapping("all")
     public Iterable  getAll() {
         return ticketBookingService.getAllItems();
     }
