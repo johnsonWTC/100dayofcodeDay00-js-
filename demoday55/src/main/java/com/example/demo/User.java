0b1227f843cca96e999195edeb546e2210ea1798
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-26 18:44:51 +0200

    js

--- demoday55/src/main/java/com/example/demo/User.java
+++ demoday55/src/main/java/com/example/demo/User.java
@@ -8,26 +8,9 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
-    public Integer getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer userID;
-
+    Integer userId;
     String userName;
+    String userEmail;
 }
--- demoday55/src/main/java/com/example/demo/UserService.java
+++ demoday55/src/main/java/com/example/demo/UserService.java
@@ -6,14 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    @Autowired
-    UserDao userDao;
 
-    public User addUser(User user){
-       return userDao.save(user);
-    }
 
-    public User getUserById(Integer userID) {
-        return userDao.findById(userID).orElse(null);
-    }
+
 }
