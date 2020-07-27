5b409a2dbccd17df8e5f5b84863cad21f484ea01
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 10:22:23 +0200

    java

--- crudday55/src/main/java/com/example/crudday55/Item.java
+++ crudday55/src/main/java/com/example/crudday55/Item.java
@@ -11,5 +11,21 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer itemID;
-    private String ItemName;
+    private String itemName;
+
+    public Integer getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(Integer itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 }
