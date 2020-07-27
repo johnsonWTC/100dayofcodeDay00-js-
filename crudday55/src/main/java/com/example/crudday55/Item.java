cccd767c01cec8f547a209208b3c1c9bbbb99f43
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 10:03:36 +0200

    java

--- crudday55/src/main/java/com/example/crudday55/Item.java
+++ crudday55/src/main/java/com/example/crudday55/Item.java
@@ -1,4 +1,15 @@
 package com.example.crudday55;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer itemID;
+    private String ItemName;
 }
