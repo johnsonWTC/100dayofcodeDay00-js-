cd9de151367cd05084394aec874629a948af4da4
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 21:00:53 +0200

    js

---
+++ day54cru/src/main/java/com/example/day54cru/Day54cruApplication.java
@@ -0,0 +1,13 @@
+package com.example.day54cru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day54cruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day54cruApplication.class, args);
+    }
+
+}
