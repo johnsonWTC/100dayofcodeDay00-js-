ab9a162e3851fb57c186390958e2b12e603a4f6d
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 10:28:28 +0200

    js

--- crudday55/src/main/java/com/example/crudday55/ItemController.java
+++ crudday55/src/main/java/com/example/crudday55/ItemController.java
@@ -1,5 +1,6 @@
 package com.example.crudday55;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("items")
 public class ItemController {
 
+    @Autowired
     Itemservice itemservice;
 
     @PostMapping("create")
--- crudday55/src/main/java/com/example/crudday55/Itemservice.java
+++ crudday55/src/main/java/com/example/crudday55/Itemservice.java
@@ -1,10 +1,12 @@
 package com.example.crudday55;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Itemservice {
 
+    @Autowired
     ItemDao itemDao;
 
     public Item createItem(Item item){
