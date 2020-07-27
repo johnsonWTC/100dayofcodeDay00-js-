a4a911c0b48d4c5a21051e36646562ddbf5f1889
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 10:09:10 +0200

    java

--- crudday55/src/main/java/com/example/crudday55/Itemservice.java
+++ crudday55/src/main/java/com/example/crudday55/Itemservice.java
@@ -1,4 +1,18 @@
 package com.example.crudday55;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Itemservice {
+
+    ItemDao itemDao;
+
+    public Item createItem(Item item){
+        return itemDao.save(item);
+    }
+
+    public Iterable findAll (){
+        return itemDao.findAll();
+    }
+
 }
