3995d9461817632984710eefc436ce8b39e51999
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-27 21:09:26 +0200

    js

--- crudday54/src/main/java/com/example/crudday54/Item.java
+++ crudday54/src/main/java/com/example/crudday54/Item.java
@@ -1,4 +1,31 @@
 package com.example.crudday54;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue
+    Integer itemId;
+    String itemName;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 }
