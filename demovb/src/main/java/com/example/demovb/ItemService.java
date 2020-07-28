ecd29378b175201613bd7039c16fb61b32d46aa8
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 21:09:41 +0200

    js

--- demovb/src/main/java/com/example/demovb/ItemService.java
+++ demovb/src/main/java/com/example/demovb/ItemService.java
@@ -1,4 +1,17 @@
 package com.example.demovb;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
+
+    ItemDao itemDao;
+
+    public Item createItem(Item item){
+        return itemDao.save(item);
+    }
+
+    public Iterable findAll(){
+        return itemDao.findAll();
+    }
 }
