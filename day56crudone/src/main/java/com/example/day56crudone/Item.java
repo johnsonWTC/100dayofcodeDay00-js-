85043343d7be8a43ae2bb65f9cc0fc3cdb5885d9
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 13:19:15 +0200

    js

--- day56crudone/src/main/java/com/example/day56crudone/Item.java
+++ day56crudone/src/main/java/com/example/day56crudone/Item.java
@@ -1,4 +1,30 @@
 package com.example.day56crudone;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer itemId;
+    String itemName;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 }
