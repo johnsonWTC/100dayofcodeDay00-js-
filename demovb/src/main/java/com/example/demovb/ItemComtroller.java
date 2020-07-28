b1ee0414cb9cfaa0cad3615bdce391263ba1b7f9
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 21:14:20 +0200

    js

--- demovb/src/main/java/com/example/demovb/ItemComtroller.java
+++ demovb/src/main/java/com/example/demovb/ItemComtroller.java
@@ -1,5 +1,8 @@
 package com.example.demovb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("items")
 public class ItemComtroller {
 
+    @Autowired
+    ItemService itemService;
 
+    @PostMapping("create")
+    public Item createItem(Item item){
+        return itemService.createItem(item);
+    }
+
+    @GetMapping("all")
+    public Iterable findAll(){
+        return itemService.findAll();
+    }
 }
