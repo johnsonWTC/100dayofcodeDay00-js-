df47fbad6ef065474dfec4bb6e0c5cce1c90d04a
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 21:03:29 +0200

    js

---
+++ demovb/src/main/java/com/example/demovb/DemovbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemovbApplication.class, args);
+    }
+
+}
---
+++ demovb/src/test/java/com/example/demovb/DemovbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
