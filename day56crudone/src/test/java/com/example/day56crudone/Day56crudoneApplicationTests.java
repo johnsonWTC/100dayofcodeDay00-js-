39027802a07a865fda90f272a9ee5825560c5a51
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 13:06:22 +0200

    js

---
+++ day56crudone/src/test/java/com/example/day56crudone/Day56crudoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day56crudone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day56crudoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
