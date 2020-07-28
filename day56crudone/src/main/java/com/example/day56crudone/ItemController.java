2387d3cdf00d8a5e2c04d6d6aa07d49e15491acb
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 13:53:20 +0200

    js

---
+++ day56crudone/src/main/java/com/example/day56crudone/ItemController.java
@@ -0,0 +1,26 @@
+package com.example.day56crudone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("items")
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @PostMapping("create")
+    public Item createItem(Item item){
+        return itemService.createItem(item);
+    }
+
+    @GetMapping("all")
+    public Iterable findAllItem(){
+        return  itemService.findAll();
+    }
+
+}
--- day56crudone/src/main/java/com/example/day56crudone/ItemService.java
+++ day56crudone/src/main/java/com/example/day56crudone/ItemService.java
@@ -16,4 +16,5 @@ public class ItemService {
     public Iterable findAll(){
         return itemDao.findAll();
     }
+
 }
