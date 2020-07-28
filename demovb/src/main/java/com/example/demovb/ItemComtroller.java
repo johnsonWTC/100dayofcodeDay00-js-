aebb9ee81e64d932f8d48da2cda566cafcbfa7d1
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-28 21:10:44 +0200

    js

---
+++ demovb/src/main/java/com/example/demovb/ItemComtroller.java
@@ -0,0 +1,11 @@
+package com.example.demovb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("items")
+public class ItemComtroller {
+
+
+}
