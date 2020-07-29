a2fd3fecc795587b36bbd2833c190645bfcadccf
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-29 16:35:38 +0200

    js

--- day55crui/src/main/java/com/example/day55crui/ItemDao.java
+++ day55crui/src/main/java/com/example/day55crui/ItemDao.java
@@ -1,4 +1,7 @@
 package com.example.day55crui;
 
-public interface ItemDao {
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemDao extends CrudRepository<Item, Integer> {
+
 }
