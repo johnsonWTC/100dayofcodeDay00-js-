cc83149de77bbfe5d45996ca3f670f04c730987f
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-30 23:01:58 +0200

    js

--- day55crui/src/main/java/com/example/day55crui/ItemController.java
+++ day55crui/src/main/java/com/example/day55crui/ItemController.java
@@ -1,4 +1,11 @@
 package com.example.day55crui;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ItemController {
+
+
 }
