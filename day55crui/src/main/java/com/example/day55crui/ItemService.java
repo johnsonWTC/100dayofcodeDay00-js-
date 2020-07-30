9a7f9abc16feb50214f583df2f2d6349ffb408f6
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-30 22:59:17 +0200

    js

--- day55crui/src/main/java/com/example/day55crui/ItemService.java
+++ day55crui/src/main/java/com/example/day55crui/ItemService.java
@@ -1,11 +1,17 @@
 package com.example.day55crui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
 
-    public Item createItem(Item item){
+    @Autowired
+    ItemDao itemDao;
 
+    public Item createItem(Item item){
+       return itemDao.save(item);
     }
+
+
 }
