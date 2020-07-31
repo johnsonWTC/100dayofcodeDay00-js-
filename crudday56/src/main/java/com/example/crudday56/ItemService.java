4ff74e4497a1f8901afc79c33a74245ccb9f021b
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-31 21:22:27 +0200

    js

--- crudday56/src/main/java/com/example/crudday56/ItemService.java
+++ crudday56/src/main/java/com/example/crudday56/ItemService.java
@@ -1,13 +1,15 @@
 package com.example.crudday56;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
 
-
+    @Autowired
+    ItemDao itemDao;
 
     public Item createItem(Item item){
-
+        return itemDao.save(item);
     }
 }
