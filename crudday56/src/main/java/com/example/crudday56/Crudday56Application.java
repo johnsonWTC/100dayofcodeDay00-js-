7f3d0d7d74173bcd18a28980d821d0c4e45c575e
Johnson Dubula <jdubula@iqbusiness.net>
2020-07-31 21:12:26 +0200

    js

---
+++ crudday56/src/main/java/com/example/crudday56/Crudday56Application.java
@@ -0,0 +1,13 @@
+package com.example.crudday56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudday56Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crudday56Application.class, args);
+    }
+
+}
---
+++ crudday56/src/main/java/com/example/crudday56/Item.java
@@ -0,0 +1,7 @@
+package com.example.crudday56;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Item {
+}
