0f669b93ec4a28b5e430c125575fe34dafaa9fd9
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-01 14:42:55 +0200

    js

--- day60/src/main/java/com/example/day60/ItemController.java
+++ day60/src/main/java/com/example/day60/ItemController.java
@@ -1,6 +1,7 @@
 package com.example.day60;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +17,11 @@ public class ItemController {
     public Item createItem(Item item){
         return itemService.createItem(item);
     }
+
+    @GetMapping("all")
+    public  Iterable findall(){
+        return itemService.findAll();
+    }
+
+
 }
