3ecde490fe9d8014f8bcaf74a4a62cb40021cfc2
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-01 14:29:00 +0200

    js

--- day60/src/main/java/com/example/day60/Item.java
+++ day60/src/main/java/com/example/day60/Item.java
@@ -1,12 +1,15 @@
 package com.example.day60;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Item {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer itemID;
     String itemName;
 
