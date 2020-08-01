28213936e1f01be9cd548f97cff26dc282d6778f
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-01 14:37:35 +0200

    js

---
+++ day60/src/main/java/com/example/day60/ItemService.java
@@ -0,0 +1,21 @@
+package com.example.day60;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemDoa itemDoa;
+
+    public Item createItem(Item item){
+        return itemDoa.save(item);
+    }
+
+    public Item editItem(Integer itemID, String oldItamName){
+        Item item = itemDoa.findById(itemID).orElse(null);
+        item.setItemName(oldItamName);
+        return itemDoa.save(item);
+    }
+}
