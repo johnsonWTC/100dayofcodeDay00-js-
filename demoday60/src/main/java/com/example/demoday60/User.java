f529709a21bc34927a2f544af046e63123ece4bb
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-02 18:35:13 +0200

    js

--- demoday60/src/main/java/com/example/demoday60/User.java
+++ demoday60/src/main/java/com/example/demoday60/User.java
@@ -1,16 +1,14 @@
 package com.example.demoday60;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer userID;
+    @Column(unique=true)
     String userName;
 
     public Integer getUserID() {
