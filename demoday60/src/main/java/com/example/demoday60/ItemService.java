83a4edc51f9aee1b562881011f798fa32669925c
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-02 17:43:19 +0200

    js

--- demoday60/src/main/java/com/example/demoday60/ItemService.java
+++ demoday60/src/main/java/com/example/demoday60/ItemService.java
@@ -1,12 +1,21 @@
 package com.example.demoday60;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
 
 
-    public User createUser(User user){
+    @Autowired
+    ItemDao itemDao;
 
+    public User createUser(User user){
+        return itemDao.save(user);
     }
+
+    public Iterable findAll(){
+        return itemDao.findAll();
+    }
+
 }
