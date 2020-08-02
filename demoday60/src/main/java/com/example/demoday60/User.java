132f3f2113152f283af1def2a82ddf9dcb0d0432
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-02 17:34:23 +0200

    js

--- demoday60/src/main/java/com/example/demoday60/User.java
+++ demoday60/src/main/java/com/example/demoday60/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
-            @GeneratedValue(strategy = GenerationType.AUTO);
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer userID;
     String userName;
 }
