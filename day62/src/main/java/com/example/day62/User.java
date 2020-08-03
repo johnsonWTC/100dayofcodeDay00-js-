b8d68de6890234ac4818ba170a94a114c834e307
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-03 05:19:30 +0200

    js

--- day62/src/main/java/com/example/day62/User.java
+++ day62/src/main/java/com/example/day62/User.java
@@ -15,4 +15,35 @@ public class User {
     String Name;
     String Surname;
 
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
 }
