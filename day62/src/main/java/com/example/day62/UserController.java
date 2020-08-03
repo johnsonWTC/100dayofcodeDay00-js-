3b251ca11465b576a7c34cbacd028b595fa03405
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-03 05:32:04 +0200

    js

--- day62/src/main/java/com/example/day62/UserController.java
+++ day62/src/main/java/com/example/day62/UserController.java
@@ -1,9 +1,16 @@
 package com.example.day62;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
+
+
+    @PostMapping("create")
+    public User createUser(User user){
+
+    }
 }
