aeb27029ca6eb41c2792dfd4f787fcc7230dec22
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-04 17:56:37 +0200

    js

--- daycrud63/src/main/java/com/example/daycrud63/UserController.java
+++ daycrud63/src/main/java/com/example/daycrud63/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("Users")
+@RequestMapping("users")
 public class UserController {
 
     UserService userService;
