1ae649a9419a46ce9756bd0bccc31556952b35d1
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-04 17:37:48 +0200

    js

--- daycrud63/src/main/java/com/example/daycrud63/UserService.java
+++ daycrud63/src/main/java/com/example/daycrud63/UserService.java
@@ -1,9 +1,12 @@
 package com.example.daycrud63;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+ UserDao userDao;
 
 }
