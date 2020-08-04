e597c0c446d1ceb27b4bcbb5950c0b67306fa619
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-04 17:46:18 +0200

    js

--- daycrud63/src/main/java/com/example/daycrud63/UserController.java
+++ daycrud63/src/main/java/com/example/daycrud63/UserController.java
@@ -1,5 +1,6 @@
 package com.example.daycrud63;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,8 @@ public class UserController {
 
     UserService userService;
 
+    @GetMapping("create")
+    public createUser(User user){
+        return userService.createUser(user);
+    }
 }
