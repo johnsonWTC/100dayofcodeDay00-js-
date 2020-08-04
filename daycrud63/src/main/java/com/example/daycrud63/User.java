373a822032791a9c5d924064a862149a237965ac
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-04 17:35:24 +0200

    js

---
+++ daycrud63/src/main/java/com/example/daycrud63/User.java
@@ -0,0 +1,41 @@
+package com.example.daycrud63;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer itemID;
+
+    String userName;
+    String name;
+
+    public Integer getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(Integer itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
