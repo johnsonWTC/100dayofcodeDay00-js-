f7f735dbc776465a5513ea79cb959a0fbd5dea8f
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-05 17:48:57 +0200

    js

--- demoday64/src/main/java/com/example/demoday64/ItemDao.java
+++ demoday64/src/main/java/com/example/demoday64/ItemDao.java
@@ -1,4 +1,6 @@
 package com.example.demoday64;
 
-public interface ItemDao {
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemDao extends CrudRepository <Item,Integer> {
 }
