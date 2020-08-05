b5c8f5da6932fac035b63a6367b924b5066984f0
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-05 17:56:32 +0200

    js

--- demoday64/src/main/java/com/example/demoday64/ItemController.java
+++ demoday64/src/main/java/com/example/demoday64/ItemController.java
@@ -1,9 +1,17 @@
 package com.example.demoday64;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("Items")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @PostMapping()
+
 }
