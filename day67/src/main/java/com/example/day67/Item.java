1bdeef5e64e2b71449bbf8c3efc9838f32c722de
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-06 14:50:33 +0200

    js

--- day67/src/main/java/com/example/day67/Item.java
+++ day67/src/main/java/com/example/day67/Item.java
@@ -13,4 +13,20 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer itemID;
     String itemName;
+
+    public Integer getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(Integer itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 }
