9ea6f3f4c24cfd0cb35f3a92879148b567538812
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-06 15:09:25 +0200

    js

--- day67/src/main/java/com/example/day67/ItemController.java
+++ day67/src/main/java/com/example/day67/ItemController.java
@@ -1,6 +1,7 @@
 package com.example.day67;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,13 @@ public class ItemController {
 
     @Autowired
     ItemService itemService;
+
     @PostMapping("create")
     public Item createItem(Item item){
         return itemService.createItem(item);
     }
+
+    @GetMapping("all")
+    public
 
 }
