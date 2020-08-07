e84bcc8366db47bf60723d47eb50fbba203d256e
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-07 12:50:03 +0200

    crud

---
+++ day67java/src/main/java/com/example/day67java/Day67javaApplication.java
@@ -0,0 +1,13 @@
+package com.example.day67java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day67javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day67javaApplication.class, args);
+    }
+
+}
