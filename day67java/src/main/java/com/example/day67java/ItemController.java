3c9e3e394dad43a0910247f543d2962420bd6f42
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-07 13:42:26 +0200

    crud

--- day67java/src/main/java/com/example/day67java/ItemController.java
+++ day67java/src/main/java/com/example/day67java/ItemController.java
@@ -1,11 +1,13 @@
 package com.example.day67java;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("items")
 public class ItemController {
 
+    @Autowired
     ItemService itemService;
 
     @PostMapping("create")
