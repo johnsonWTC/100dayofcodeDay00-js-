96ad504a35bdc062d1e7ebd002d89bccb77fa9f9
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-07 13:17:37 +0200

    crud

--- day67java/src/main/java/com/example/day67java/ItemController.java
+++ day67java/src/main/java/com/example/day67java/ItemController.java
@@ -1,4 +1,17 @@
 package com.example.day67java;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("items")
 public class ItemController {
+
+    ItemService itemService;
+
+    @GetMapping("create")
+    public Item createItem(Item item){
+        return itemService.createItem();
+    }
 }
