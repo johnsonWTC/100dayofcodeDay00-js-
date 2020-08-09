b4ca7b01034c6d86351a9eed8f7b91990709504f
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-09 22:54:55 +0200

    js

--- demoday65/src/main/java/com/example/demoday65/ItemService.java
+++ demoday65/src/main/java/com/example/demoday65/ItemService.java
@@ -1,8 +1,12 @@
 package com.example.demoday65;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    private ItemDao itemDao;
 }
