c49dc8fbb7e6ff22e6f491e45979e115d96f11d5
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-10 19:24:38 +0200

    day 70

--- day70crud/src/main/java/com/example/day70crud/Item.java
+++ day70crud/src/main/java/com/example/day70crud/Item.java
@@ -1,7 +1,13 @@
 package com.example.day70crud;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 }
