6e1b40785940d89946ff5f3de32a034c7d22c1c3
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-10 19:23:32 +0200

    day 70

---
+++ day70crud/src/test/java/com/example/day70crud/Day70crudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day70crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day70crudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
