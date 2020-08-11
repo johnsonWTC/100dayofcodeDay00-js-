6bd88e3674bb5c368da287614f6e08d5549b49af
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-11 18:03:33 +0200

    js

--- demoday71/src/main/java/com/example/demoday71/Item.java
+++ demoday71/src/main/java/com/example/demoday71/Item.java
@@ -2,7 +2,13 @@ package com.example.demoday71;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 }
