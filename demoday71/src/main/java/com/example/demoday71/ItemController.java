27a36a13720011eb5fe8af4dacff6976d4c0dca6
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-11 18:35:40 +0200

    js

--- demoday71/src/main/java/com/example/demoday71/ItemController.java
+++ demoday71/src/main/java/com/example/demoday71/ItemController.java
@@ -1,9 +1,12 @@
 package com.example.demoday71;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("items")
 public class ItemController {
+
+    @PostMapping("create")
 }
