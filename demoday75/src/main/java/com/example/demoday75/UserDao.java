e309a187cd2ed59002db920f10064743dd4a2156
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-12 19:53:21 +0200

    js

--- demoday75/src/main/java/com/example/demoday75/UserDao.java
+++ demoday75/src/main/java/com/example/demoday75/UserDao.java
@@ -2,5 +2,5 @@ package com.example.demoday75;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserDao extends CrudRepository<> {
+public interface UserDao extends CrudRepository<User,Integer> {
 }
