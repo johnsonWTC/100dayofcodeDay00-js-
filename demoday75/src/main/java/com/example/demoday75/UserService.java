75ecb895648f651eab55dab2d09085c96fc10264
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-12 19:57:31 +0200

    js

--- demoday75/src/main/java/com/example/demoday75/UserService.java
+++ demoday75/src/main/java/com/example/demoday75/UserService.java
@@ -1,7 +1,11 @@
 package com.example.demoday75;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserDao userDao;
 }
