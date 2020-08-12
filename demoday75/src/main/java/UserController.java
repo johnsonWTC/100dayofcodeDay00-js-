e525dcd9baf2aad6689bd2ce72d0cbc1589ef070
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-12 20:02:56 +0200

    js

--- demoday75/src/main/java/UserController.java
+++ demoday75/src/main/java/UserController.java
@@ -1,7 +1,14 @@
+import com.example.demoday75.User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
     @RestController
     @RequestMapping("users")
+
+    @PostMapping("create")
+    public User createUser(User user){
+
+    }
 }
