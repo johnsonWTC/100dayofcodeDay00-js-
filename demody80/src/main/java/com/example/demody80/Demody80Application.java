1587b35a0051c77d78c93aaa9d1ec896e398d94c
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-17 22:26:34 +0200

    js

---
+++ demody80/src/main/java/com/example/demody80/Demody80Application.java
@@ -0,0 +1,13 @@
+package com.example.demody80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demody80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demody80Application.class, args);
+    }
+
+}
---
+++ demody80/src/test/java/com/example/demody80/Demody80ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demody80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demody80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
