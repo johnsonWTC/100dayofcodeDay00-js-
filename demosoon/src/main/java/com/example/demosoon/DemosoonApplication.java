fc7ae814b6011987fd4535555cffe949259b4742
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-18 17:40:18 +0200

    js

---
+++ demosoon/src/main/java/com/example/demosoon/DemosoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosoonApplication.class, args);
+    }
+
+}
---
+++ demosoon/src/test/java/com/example/demosoon/DemosoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
