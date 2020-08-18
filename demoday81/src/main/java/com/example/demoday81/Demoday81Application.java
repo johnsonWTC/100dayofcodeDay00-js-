494eccccd3a373badb2d73e25e36e6de1ece2ef5
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-18 18:58:47 +0200

    js

---
+++ demoday81/src/main/java/com/example/demoday81/Demoday81Application.java
@@ -0,0 +1,13 @@
+package com.example.demoday81;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoday81Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoday81Application.class, args);
+    }
+
+}
---
+++ demoday81/src/test/java/com/example/demoday81/Demoday81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoday81;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoday81ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
