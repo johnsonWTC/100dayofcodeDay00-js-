9e081d33aebab210d660b3b2db003193bcf2f737
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-19 19:19:55 +0200

    js

--- demosoon/src/main/java/com/example/demosoon/UserController.java
+++ demosoon/src/main/java/com/example/demosoon/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demosoon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("users")
 public class UserController {
+
+    @Autowired
+    UserService userService
 
     @GetMapping("create")
     public User createUser(User user){
