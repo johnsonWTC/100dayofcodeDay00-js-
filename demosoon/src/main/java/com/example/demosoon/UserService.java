90f4df2587bb0deaaf23c7779947a1290d7449f5
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-19 19:44:32 +0200

    js

--- demosoon/src/main/java/com/example/demosoon/UserService.java
+++ demosoon/src/main/java/com/example/demosoon/UserService.java
@@ -1,11 +1,13 @@
 package com.example.demosoon;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
     private  UserDao userDao;
 
     public  User CreateUser(User user){
