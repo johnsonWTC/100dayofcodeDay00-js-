91e0c594605abab41d69e2772ac2ef02e54ce70e
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-20 14:18:01 +0200

    js

--- demoday90/src/main/java/com/example/demoday90/itemController.java
+++ demoday90/src/main/java/com/example/demoday90/itemController.java
@@ -1,4 +1,9 @@
 package com.example.demoday90;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class itemController {
 }
