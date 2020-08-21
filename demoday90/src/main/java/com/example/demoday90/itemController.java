537b2fc55e683097b3884f9dcd47ca32e6e5de9b
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-21 09:42:09 +0200

    js

--- demoday90/src/main/java/com/example/demoday90/itemController.java
+++ demoday90/src/main/java/com/example/demoday90/itemController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("users")
 public class itemController {
 
     @Autowired
