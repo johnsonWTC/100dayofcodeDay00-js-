dad26b93d78ed4ee5fe5c1c1f8b2654219468315
Johnson Dubula <jdubula@iqbusiness.net>
2020-08-22 18:28:40 +0200

    js and c sharp

---
+++ dotnetapp/Models/Movies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public class Class
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
