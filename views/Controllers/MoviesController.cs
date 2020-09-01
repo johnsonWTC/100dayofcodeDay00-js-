5b9d59c479244bc333f94a5c9e93d20f3ac9064a
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-02 00:55:43 +0200

    day 100

--- views/Controllers/MoviesController.cs
+++ views/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using views.Models;
+using views.NewFolder;
 
 namespace views.Controllers
 {
@@ -12,8 +13,24 @@ namespace views.Controllers
         public IActionResult Random()
         {
             var movie = new Movie() {name = "The rise of jam"};
-            
-            return View(movie);
+            ViewData["Movies"] = movie;
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Johnson" },
+                new Customer { Name = "Thandwelihle" },
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+
+            return View(viewModel);
         }
+
+       
     }
 }
