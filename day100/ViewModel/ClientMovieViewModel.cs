41a9820e8b96914687985b8711e0e9a7318c5f6f
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-02 01:16:27 +0200

    day 100

--- day100/ViewModel/ClientMovieViewModel.cs
+++ day100/ViewModel/ClientMovieViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using day100.Models;
 
 namespace day100.ViewModel
 {
     public class ClientMovieViewModel
     {
+
     }
 }
