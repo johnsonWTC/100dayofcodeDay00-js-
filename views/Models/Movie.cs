4899c8da7393c1bd25b558a1bee1a5188d66f7d9
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-01 07:56:02 +0200

    mvc

---
+++ views/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace views.Models
+{
+    public class Movie
+    {
+        public string name { get; set; }
+    }
+}
