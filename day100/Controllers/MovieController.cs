c0410809ce68a69c76c1936ada1d5934685ed404
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-02 02:04:26 +0200

    js

--- day100/Controllers/MovieController.cs
+++ day100/Controllers/MovieController.cs
@@ -20,12 +20,13 @@ namespace day100.Controllers
             clientList.Add(johnson);
             Clients thandwelihle = new Clients();
             thandwelihle.ClientName = "thandwelihle";
+            clientList.Add(thandwelihle);
             ClientMovieViewModel clientMovieViewModel = new ClientMovieViewModel();
             clientMovieViewModel.movie = movie;
             clientMovieViewModel.clients = clientList;
        
 
-            return View(movie);
+            return View(clientMovieViewModel);
 
         }
     }
