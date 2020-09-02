40e9f8bf055861bd4a23950fa8e5a9e424990397
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-02 08:32:04 +0200

    js

---
+++ day100two/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace day100two.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
