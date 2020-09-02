b0586844a344d2b05875e57515c016c7f6c63a5f
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-02 08:38:16 +0200

    js

---
+++ day100two/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using day100two.Models;
+
+namespace day100two.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
