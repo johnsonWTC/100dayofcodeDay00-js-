db3c66786e11320ec54224c535614c59908a85f0
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-10 10:54:24 +0200

    init

--- SimpleImageGallery.Services/ImaageService.cs
+++ SimpleImageGallery.Services/ImaageService.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using SimpleImageGallery.Data;
+using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
 
 namespace SimpleImageGallery.Services
 {
     public class ImaageService : Image
     {
+        private readonly SimpleImageGalleryDbContext _context;
+
+        public ImaageService(SimpleImageGalleryDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<GalleryImage> GellAll()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public GalleryImage GetGalleryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GalleryImage> GetWithTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
