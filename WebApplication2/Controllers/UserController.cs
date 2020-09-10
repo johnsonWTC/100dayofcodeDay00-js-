828e8de48f0ba2bc494d66a5992a0781b8eba377
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-10 09:17:40 +0200

    js

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
