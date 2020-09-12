bfa0b69521712fcdbd37c898f9186b83c0dffd6d
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-12 16:33:26 +0200

    init

--- SimpleImageGallery.Data/Models/GalleryImage.cs
+++ SimpleImageGallery.Data/Models/GalleryImage.cs
@@ -8,6 +8,6 @@ namespace SimpleImageGallery.Data.Models
     {
         public  string Title { get; set; }
         public DateTime Created { get; set; }
-        public string MyProperty { get; set; }
+        public string url { get; set; }
     }
 }
