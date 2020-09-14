d8f318f2298e4e2b0e23d44cde16eebd12034532
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-14 21:25:06 +0200

    js

--- WebApplication3/Controllers/ItemController.cs
+++ WebApplication3/Controllers/ItemController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication3.Models;
+using WebApplication3.ModelView;
 
 namespace WebApplication3.Controllers
 {
@@ -13,10 +14,17 @@ namespace WebApplication3.Controllers
         {
             Items book = new Items();
             book.itemName = "book";
+            Items laptop = new Items();
+            laptop.itemName = "laptop";
+
+
+            ItemModelView itemModelView = new ItemModelView();
+            itemModelView.Items.Add(book);
+            itemModelView.Items.Add(laptop);
           
 
 
-            return View(book);
+            return View(itemModelView);
         }
     }
 }
