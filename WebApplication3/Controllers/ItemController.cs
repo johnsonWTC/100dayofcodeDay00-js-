42fa7575c4d16c9781d3d5c7cc4f2f8077e540f6
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-14 21:09:04 +0200

    js

---
+++ WebApplication3/Controllers/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            Items book = new Items();
+            book.itemName = "book";
+            List<Items> items = new List<Items>();
+
+            return View(book);
+        }
+    }
+}
