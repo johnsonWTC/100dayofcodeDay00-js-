687a3cceec86a9a206c418d065cf4e6870ee1091
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-16 18:44:58 +0200

    21 day commit me

---
+++ WebApplication4/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class TestViewModel
+    {
+        public List<Item> ListOfItems { get; set; }
+        public List<User> ListOfUser { get; set; }
+    }
+}
