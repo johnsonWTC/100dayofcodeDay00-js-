a70ab53125dcdcea3f88d0f3a9aa9786a9dc9ad6
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-17 23:22:21 +0200

    js

---
+++ WebApplication6/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public class Item
+    {
+        public int itemID { get; set; }
+        public string itemName { get; set; }
+    }
+}
