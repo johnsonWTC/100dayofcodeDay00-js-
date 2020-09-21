817644e8efbe43fdbb0f18821f1c8a8b6a71f634
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-21 10:36:59 +0200

    js

---
+++ WebApplication7/ViewModel/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Models;
+
+namespace WebApplication7.ViewModel
+{
+    public class UserViewModel
+    {
+        public User user { get; set; }
+        public Item item { get; set; }
+    }
+}
