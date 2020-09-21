a08aea2204acae1c12ef2f303abc023447286ea7
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-21 15:16:28 +0200

    init

--- SimpleImageGallary/Models/GalleryIndelModel.cs
+++ SimpleImageGallary/Models/GalleryIndelModel.cs
@@ -8,6 +8,6 @@ namespace SimpleImageGallary.Models
     public class GalleryIndelModel
     {
         public IEnumerable<GalleryImage> Images { get; set; }
-        public int MyProperty { get; set; }
+        public string MyProperty { get; set; }
     }
 }
