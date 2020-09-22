843fa156bf75bdcb12b8ec06e0799610d0198055
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-22 09:07:48 +0200

    asp.net

--- WebApplication8/ViewModel/UserViewModel.cs
+++ WebApplication8/ViewModel/UserViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication8.Models;
 
 namespace WebApplication8.ViewModel
 {
     public class UserViewModel
     {
+        public List<User> Users { get; set; }
     }
 }
