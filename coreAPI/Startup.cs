8ddeeb62ddb27e973c1b11db20b5d518d03a6cfe
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-27 09:53:52 +0200

    js

--- coreAPI/Startup.cs
+++ coreAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace coreAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+      //      app.UseHttpsRedirection();
 
             app.UseRouting();
 
