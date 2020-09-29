db1d45ea6164e4b5f71aa9be5fdaf6f2efbfb0cf
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-29 09:47:02 +0200

    api

---
+++ WapAPI6/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WapAPI6.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WapAPI6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public static List<User> users = new List<User>();
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public List<User> Get()
+        {
+            return users;
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            User user = users.FirstOrDefault(e => e.userID == id);
+            return user;
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            users.Add(user);
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}/{newName}")]
+        public void Put(int id, string newName)
+        {
+            User user = users.FirstOrDefault(e => e.userID == id);
+            user.userName = newName;
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
