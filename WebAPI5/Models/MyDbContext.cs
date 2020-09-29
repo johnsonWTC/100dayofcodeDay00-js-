0e20a162a4a44fa687c43c968daa526ea8698b73
Johnson Dubula <jdubula@iqbusiness.net>
2020-09-29 08:08:16 +0200

    api

---
+++ WebAPI5/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI5.Models
+{
+    public class MyDbContext : DbContext
+    {
+        
+    
+            public MyDbContext()
+            {
+            }
+
+            public DbSet<User> Users { get; set; }
+
+        }
+    }
