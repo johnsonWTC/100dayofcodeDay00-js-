98dddd744c821b76f7a790c5b96653676d6e3c9f
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-04 23:55:03 +0200

    api

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -49,6 +49,7 @@ namespace API.Controllers
         public void Delete(int id)
         {
             User user = users.FirstOrDefault(e => e.userID == id);
+            
             users.Remove(user);
         }
     }
