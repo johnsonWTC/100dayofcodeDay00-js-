0ddd1c99e64af5bd502403240298bcaf8da30b31
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-05 22:43:02 +0200

    api

---
+++ API14/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API14.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+    }
+}
