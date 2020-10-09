cc6072c01a8c858efe8b7ad581b6cd9bf39d8530
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-09 09:08:15 +0200

    api

--- API21/Controllers/UserController.cs
+++ API21/Controllers/UserController.cs
@@ -16,9 +16,9 @@ namespace API21.Controllers
        public static List<User> users = new List<User>();
         // GET: api/<UserController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return users;
         }
 
         // GET api/<UserController>/5
