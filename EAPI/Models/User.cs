44cab88444c4011efff7fdabeeddcc487cec8a3e
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-10 22:29:28 +0200

    jamizzy

---
+++ EAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAPI.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+    }
+}
