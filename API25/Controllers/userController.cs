95e0bfbc7756d36109b3e45ffbaf507c2dce3027
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-12 19:13:34 +0200

    api

--- API25/Controllers/userController.cs
+++ API25/Controllers/userController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API25.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,16 +17,18 @@ namespace API25.Controllers
 
         public static List<User> users = new List<User>();
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return users;
         }
 
         // GET api/<userController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public User Get(int id)
         {
-            return "value";
+            User user = users.FirstOrDefault(e => e.userID == id);
+            
+            return user;
         }
 
         // POST api/<userController>
