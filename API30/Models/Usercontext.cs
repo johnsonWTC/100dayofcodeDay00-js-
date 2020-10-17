b4a66fcaa9a3d066d2c3cb37e90e086c156a0ace
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-18 00:54:36 +0200

    api 31

---
+++ API30/Models/Usercontext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API30.Models
+{
+    public class Usercontext : DbContext
+    {
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ASSET-10422\\SQLEXPRESS;Initial Catalog=New;Integrated Security=True");
+            // optionsBuilder.UseSqlServer("server=.;database=dataTest;trusted_connection=true;");
+            // "Data Source=ASSET-10422\\SQLEXPRESS;Initial Catalog=New;Integrated Security=True"
+        }
+
+    }
+}
