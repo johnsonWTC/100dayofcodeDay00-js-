9260c7ced5ff89160704b81384506701ab836d17
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-20 23:47:24 +0200

    api

---
+++ API33/Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace API29.Models
+{
+    public class Orders
+    {
+
+        public int id { get; set; }
+        public DateTime orderPlaced { get; set; }
+        public DateTime ? orderFullFilled { get; set; }
+
+        public int customerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public ICollection<ProductOrder> ProductOrder { get; set; }
+    }
+}
---
+++ API33/Models/ProductOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace API33.Models
+{
+    public class ProductOrder
+    {
+        public int id { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Orders Orders { get; set; }
+        public Product Product { get; set; }
+    }
+}
