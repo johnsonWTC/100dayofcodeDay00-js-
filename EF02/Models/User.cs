796d18475ee251732a68e392feb32793df0a50d8
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-23 18:51:23 +0200

    api

---
+++ EF02/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF02.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string userName { get; set; }
+    }
+}
