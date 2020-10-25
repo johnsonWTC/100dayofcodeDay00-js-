3ca54252d27d063f0d78011c0d40ea4dde5bf5f1
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-25 21:30:24 +0200

    API

---
+++ EF05/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF05.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+    }
+}
