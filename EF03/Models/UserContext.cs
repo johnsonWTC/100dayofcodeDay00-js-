a112f15fb699bbf4c212a34c53a9518ee12214d9
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-25 21:31:00 +0200

    API

--- EF03/Models/UserContext.cs
+++ EF03/Models/UserContext.cs
@@ -13,8 +13,6 @@ namespace EF03.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE03;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            // optionsBuilder.UseSqlServer("server=.;database=dataTest;trusted_connection=true;");
-            // "Data Source=ASSET-10422\\SQLEXPRESS;Initial Catalog=New;Integrated Security=True"
         }
     }
 }
