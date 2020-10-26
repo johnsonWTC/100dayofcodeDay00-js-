ee82c57d2d46130e1dff8ea3794f43f68c3f9443
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-26 19:21:17 +0200

    EF06

--- EF05/Models/UserContext.cs
+++ EF05/Models/UserContext.cs
@@ -8,11 +8,11 @@ namespace EF05.Models
 {
     public class UserContext :DbContext
     {
-        public DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<User> Users { get; set; } protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE05;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
+
+       
     }
 }
