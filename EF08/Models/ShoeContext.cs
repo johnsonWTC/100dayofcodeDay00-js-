9aadfe4f510f2f6fbaf761f10fe97b2740998af1
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-29 20:28:26 +0200

    API

--- EF08/Models/ShoeContext.cs
+++ EF08/Models/ShoeContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EF08.Models
 {
-    public class ShoeContext
+    public class ShoeContext :DbContext
     {
+         public DbSet<Shoe> shoes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE08;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
