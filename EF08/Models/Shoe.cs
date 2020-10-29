ce69510b07a3892d0ced1115f5dbe12024c89773
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-29 20:22:29 +0200

    api

---
+++ EF08/Models/Shoe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF08.Models
+{
+    public class Shoe
+    {
+        public int shoesID { get; set; }
+        public string shoeName { get; set; }
+    }
+}
