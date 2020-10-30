f3b869ec6e1be660fde497b13f62b6a3b633c743
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-30 20:06:15 +0200

    api

---
+++ EF09/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF09.Models
+{
+    public class Book
+    {
+        public int bookID { get; set; }
+    }
+}
