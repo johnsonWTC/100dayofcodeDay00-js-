a9e469c29264c3de6c1693fa36d0d7a0eb7c91f8
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-30 20:17:18 +0200

    api

---
+++ EF09/Controllers/BookController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EF09.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EF09.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        BookContext bookContext = new BookContext();
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return bookContext.Books.ToList();
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            Book book = bookContext.Books.FirstOrDefault(e => e.bookID == id);
+            return book;
+
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
