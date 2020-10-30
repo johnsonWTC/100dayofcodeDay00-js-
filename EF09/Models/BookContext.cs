901198d25859ba33ca9961ffaa0ed027e959041a
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-30 20:12:30 +0200

    api

--- EF09/Models/BookContext.cs
+++ EF09/Models/BookContext.cs
@@ -9,5 +9,10 @@ namespace EF09.Models
     public class BookContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE05;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
