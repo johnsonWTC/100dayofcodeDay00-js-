6dca05e5e64a528f6b88486baf2e25440ae7978a
Johnson Dubula <jdubula@iqbusiness.net>
2020-10-31 04:36:47 +0200

    api

---
+++ EF10/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF10.Models
+{
+    public class Department
+    {
+        public int departmentID { get; set; }
+        public string departmentName { get; set; }
+    }
+}
