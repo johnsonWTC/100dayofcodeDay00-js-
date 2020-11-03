e1ca964c2689c21fe5aad92c5d9654625e1af116
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-03 17:16:18 +0200

    API

--- WAPI12/Models/Faculty.cs
+++ WAPI12/Models/Faculty.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 
 namespace WAPI12.Models
 {
     public class Faculty
     {
+        public int FacultyID { get; set; }
+        public string FacultyName { get; set; }
     }
 }
