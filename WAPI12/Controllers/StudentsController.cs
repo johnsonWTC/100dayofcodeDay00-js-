511ba4f3a79f145f507936b9b97e1fcd5cd9bdb9
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-03 18:49:56 +0200

    API

--- WAPI12/Controllers/StudentsController.cs
+++ WAPI12/Controllers/StudentsController.cs
@@ -26,7 +26,8 @@ namespace WAPI12.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            Student student = _context.students.FirstOrDefault(e => e.studentID == id);
+            return student;
         }
 
         // POST api/<StudentsController>
