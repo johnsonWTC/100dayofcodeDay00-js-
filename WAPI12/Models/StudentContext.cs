6d9bc252a06e960d7e83bbc4cbd3dccca3262ebd
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-03 17:22:45 +0200

    API

---
+++ WAPI12/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAPI12.Models
+{
+    public class StudentContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Faculty> faculties { get; set; }
+    }
+}
