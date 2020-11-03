cc4ffd6f3396653a4934e9812b36d8e2faf81639
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-03 17:23:59 +0200

    API

--- WAPI12/Models/StudentContext.cs
+++ WAPI12/Models/StudentContext.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace WAPI12.Models
 {
-    public class StudentContext
+    public class StudentContext : DbContext
     {
         public DbSet<Student> students { get; set; }
         public DbSet<Faculty> faculties { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE11;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
