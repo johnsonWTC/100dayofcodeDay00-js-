e3653bc1b70c7bcdd5d35f765fb75a973ad3e0d9
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-04 23:31:42 +0200

    API

---
+++ EF12/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF12.Models
+{
+    public class Department
+    {
+        public int departmentID { get; set; }
+        public string departmentName { get; set; }
+    }
+}
