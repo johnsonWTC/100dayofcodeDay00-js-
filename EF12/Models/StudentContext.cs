3473f6185bf2d083558afe5d1264f4740dd77f0a
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-04 23:35:05 +0200

    API

--- EF12/Models/StudentContext.cs
+++ EF12/Models/StudentContext.cs
@@ -8,5 +8,7 @@ namespace EF12.Models
 {
     public class StudentContext :DbContext
     {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
     }
 }
