32e2367346628b331b42eb71f2e8fe0763c2d3bc
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-04 23:43:03 +0200

    API

---
+++ EF12/Migrations/20201104213715_init.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EF12.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    studentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    studentName = table.Column<string>(nullable: true),
+                    departmentID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.studentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    departmentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    departmentName = table.Column<string>(nullable: true),
+                    studentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.departmentID);
+                    table.ForeignKey(
+                        name: "FK_Departments_Students_studentID",
+                        column: x => x.studentID,
+                        principalTable: "Students",
+                        principalColumn: "studentID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_studentID",
+                table: "Departments",
+                column: "studentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
--- EF12/Models/StudentContext.cs
+++ EF12/Models/StudentContext.cs
@@ -10,5 +10,10 @@ namespace EF12.Models
     {
         public DbSet<Student> Students { get; set; }
         public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE12;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
