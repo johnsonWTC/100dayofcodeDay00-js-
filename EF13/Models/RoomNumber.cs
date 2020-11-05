4cb9fb4f8ae6122d9b26d7c98d34cad215c7a20a
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-05 08:31:10 +0200

    API

---
+++ EF13/Models/RoomNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF13.Models
+{
+    public class RoomNumber
+    {
+    }
+}
---
+++ EF13/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF13.Models
+{
+    public class Student
+    {
+        public int  studentID  { get; set; }
+        public string studentName { get; set; }
+
+        public int roomNumberID { get; set; }
+
+        public RoomNumber roomNumber  { get; set; }
+
+
+    }
+}
