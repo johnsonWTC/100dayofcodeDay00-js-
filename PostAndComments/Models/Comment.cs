d72beaf259353e5ee941be2fffc406be95ce0d3e
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-08 01:11:26 +0200

    api

--- PostAndComments/Models/Comment.cs
+++ PostAndComments/Models/Comment.cs
@@ -9,7 +9,5 @@ namespace PostAndComments.Models
     {
         public int commentID { get; set; }
         public string comment { get; set; }
-        public Post post { get; set; }
-        public int postID { get; set; }
     }
 }
