3311869df93be0352921611f80c3f054189467e8
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-08 00:31:16 +0200

    API

--- PostAndComments/Models/postContext.cs
+++ PostAndComments/Models/postContext.cs
@@ -11,7 +11,15 @@ namespace PostAndComments.Models
         public postContext(DbContextOptions<postContext> options): base(options)
         {
         }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=post;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
     }
-}
 
     
