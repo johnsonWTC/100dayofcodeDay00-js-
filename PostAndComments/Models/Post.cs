9a9242e5effd5a4f4e8d4a1233c16eec3d91d96f
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-08 00:00:27 +0200

    API

---
+++ PostAndComments/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostAndComments.Models
+{
+    public class Post
+    {
+        public int postID { get; set; }
+        public string postContent { get; set; }
+        public List<Comments> Comments { get; set; }
+    }
+}
