df41c9ee1769a93e290ef4a62f2dd7bfa8ecf3f2
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-08 07:57:56 +0200

    api

--- PostAndComments/Migrations/20201108055445_initi.Designer.cs
+++ PostAndComments/Migrations/20201108055445_initi.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PostAndComments.Models;
 
 namespace PostAndComments.Migrations
 {
     [DbContext(typeof(postContext))]
-    partial class postContextModelSnapshot : ModelSnapshot
+    [Migration("20201108055445_initi")]
+    partial class initi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PostAndComments/Migrations/20201108055445_initi.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostAndComments.Migrations
+{
+    public partial class initi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_Posts_postID",
+                table: "comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "postID",
+                table: "comments",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_Posts_postID",
+                table: "comments",
+                column: "postID",
+                principalTable: "Posts",
+                principalColumn: "postID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_Posts_postID",
+                table: "comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "postID",
+                table: "comments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_Posts_postID",
+                table: "comments",
+                column: "postID",
+                principalTable: "Posts",
+                principalColumn: "postID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
