4bd2def80fb86daaef3c361b48b6f5087038a1bc
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-08 08:13:46 +0200

    api

---
+++ EF15/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF15.Models
+{
+    public class Post
+    {
+        public int postID { get; set; }
+        public string postContent { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
