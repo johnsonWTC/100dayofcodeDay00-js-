7c4f4a88a7344cf3a4c8c46dc38b02cf8f85fa26
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-09 23:34:24 +0200

    posts

--- Post)2/Models/Post.cs
+++ Post)2/Models/Post.cs
@@ -10,6 +10,7 @@ namespace Post_2.Models
         public int postID { get; set; }
         public string postContent { get; set; }
 
-        public comments comments { get; set; }
+        
+
     }
 }
