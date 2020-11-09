c6f7d56fa7404088d0876884715acdf6f7552f7d
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-09 23:48:00 +0200

    posts

--- Post)2/Models/PostContext.cs
+++ Post)2/Models/PostContext.cs
@@ -8,5 +8,12 @@ namespace Post_2.Models
 {
     public class PostContext : DbContext
     {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<comments> comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=post02;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
--- Post)2/Startup.cs
+++ Post)2/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Post_2.Models;
 
 namespace Post_2
 {
@@ -24,6 +25,8 @@ namespace Post_2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<PostContext>(options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=posts02;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
