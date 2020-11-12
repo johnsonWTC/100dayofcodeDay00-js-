2f1566581344c4caecbddd3edf800c005721039b
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-12 09:09:34 +0200

    api

--- Post)2/Models/Post.cs
+++ Post)2/Models/Post.cs
@@ -8,7 +8,10 @@ namespace Post_2.Models
     public class Post
     {
         public int postID { get; set; }
+        public string postTitle { get; set; }
         public string postContent { get; set; }
+
+        public DateTime timeUpdate { get; set; }
 
         public List<comment> comments { get; set; }
 
