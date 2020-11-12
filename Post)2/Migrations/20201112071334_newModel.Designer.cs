c134c97d927b8930c69a15873935639ca2e20ed2
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-12 09:48:56 +0200

    api

--- Post)2/Migrations/20201112071334_newModel.Designer.cs
+++ Post)2/Migrations/20201112071334_newModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Post_2.Models;
 
 namespace Post_2.Migrations
 {
     [DbContext(typeof(PostContext))]
-    partial class PostContextModelSnapshot : ModelSnapshot
+    [Migration("20201112071334_newModel")]
+    partial class newModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
