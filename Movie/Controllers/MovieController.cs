7e1faab544974822749a107e6b8e43dcd9bd3cf5
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-22 16:59:39 +0200

    init

--- Movie/Controllers/MovieController.cs
+++ Movie/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
 
 namespace Movie.Controllers
 {
@@ -10,7 +11,7 @@ namespace Movie.Controllers
     {
         public IActionResult Random()
         {
-            var movie = 
+            Movie m
             return View();
         }
     }
