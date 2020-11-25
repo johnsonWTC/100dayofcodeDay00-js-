d7220ca04e5bffd99c6eef6e9cf5fbe1b85de980
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-25 17:03:35 +0200

    init

--- Movie/Controllers/MovieController.cs
+++ Movie/Controllers/MovieController.cs
@@ -12,7 +12,7 @@ namespace Movie.Controllers
         public IActionResult Random()
         {
             Show show = new Show();
-            show.Name
+            show.Name =  ""
             return View();
         }
     }
