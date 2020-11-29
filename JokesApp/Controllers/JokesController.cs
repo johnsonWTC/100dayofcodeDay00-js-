168ffd698c5d248b313d07c3267caa66e5025766
Johnson Dubula <jdubula@iqbusiness.net>
2020-11-29 02:55:57 +0200

    init

--- JokesApp/Controllers/JokesController.cs
+++ JokesApp/Controllers/JokesController.cs
@@ -25,7 +25,7 @@ namespace JokesApp.Controllers
             return View(await _context.Joke.ToListAsync());
         }
 
-        public async Task<IActionResult> ShowSearchForm()
+        public IActionResult ShowSearchForm()
         {
             return View();
         }
