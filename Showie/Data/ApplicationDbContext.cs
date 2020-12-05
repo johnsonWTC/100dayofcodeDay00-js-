f61fbddd4c788219508251db7eba4e56f5ea2fc4
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-05 02:26:32 +0200

    init

--- Showie/Data/ApplicationDbContext.cs
+++ Showie/Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Showie.Data
         public DbSet<Movie> movies { get; set; }
         public DbSet<Customer> customers { get; set; }
         public DbSet<MemberShipType> memberShipTypes { get; set; }
-        public DbSet<Movie> Movie { get; set; }
+        public DbSet<FollowTable> followTables { get; set; }
         public DbSet<Movie> Movie { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
