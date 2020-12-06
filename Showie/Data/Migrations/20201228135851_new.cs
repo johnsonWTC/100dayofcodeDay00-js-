28478b537905291943300c4ce3e4a546512b41b5
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-06 15:59:12 +0200

    init

---
+++ Showie/Data/Migrations/20201228135851_new.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Showie.Data.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "followTables");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "followTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FollowedID = table.Column<int>(type: "int", nullable: false),
+                    FollowerID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_followTables", x => x.Id);
+                });
+        }
+    }
+}
