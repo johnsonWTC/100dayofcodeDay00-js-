a08742f9156ad3d4951ea681d69150e184844606
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-06 02:26:06 +0200

    init

--- Showie/Data/ApplicationDbContext.cs
+++ Showie/Data/ApplicationDbContext.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Showie.Models;
 
 namespace Showie.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<MemberShipType> memberShipTypes { get; set; }
+        public DbSet<Movie> Movie { get; set; }
         public DbSet<Movie> Movie { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
