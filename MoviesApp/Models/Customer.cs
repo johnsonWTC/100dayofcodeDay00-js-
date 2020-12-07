edabdbb06cfe721163354dd5e313dd1cd0575366
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-08 01:39:48 +0200

    init

--- MoviesApp/Models/Customer.cs
+++ MoviesApp/Models/Customer.cs
@@ -7,5 +7,12 @@ namespace MoviesApp.Models
 {
     public class Customer
     {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public bool isSubsribedToNewsLetter { get; set; }
+
+        public MemberShipType MemberShipType { get; set; }
+
+        public short MemberShipTypeId { get; set; }
     }
 }
