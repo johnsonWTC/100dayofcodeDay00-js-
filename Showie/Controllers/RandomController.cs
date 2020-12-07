3a64d88a96a75a965c7c32c3a38acfc6b35dbe02
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-07 02:13:46 +0200

    init

---
+++ Showie/Controllers/RandomController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Showie.Models;
+using Showie.ShowViewModel;
+
+namespace Showie.Controllers
+{
+    public class RandomController : Controller
+    {
+        // GET: RandomController
+        public ActionResult Random(int id)
+        {
+            var movie = new Movie();
+            movie.Name = "Age Of Altron";
+
+            var movieTwo = new Movie();
+            movieTwo.Name = "Winter Soldier";
+
+
+
+            var customer = new Customer();
+            customer.Name = "Johnson";
+
+
+            var customerTwo = new Customer();
+            customerTwo.Name = "Thandwelihle";
+
+
+            List<Customer> customers = new List<Customer>();
+            customers.Add(customer);
+            customers.Add(customerTwo);
+
+
+
+
+
+            ShowViewModel viewModel = new ShowViewModel();
+            viewModel.Movie = movie;
+            viewModel.Customers = customers;
+
+            return View(viewModel);
+        }
+
+        // GET: RandomController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RandomController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RandomController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RandomController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RandomController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RandomController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RandomController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Showie/Models/Movie.cs
+++ Showie/Models/Movie.cs
@@ -7,5 +7,7 @@ namespace Showie.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
