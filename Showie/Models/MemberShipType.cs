5f627918abdbfa9cf397a51ac205e4745c4ad82d
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-07 02:01:51 +0200

    init

--- Showie/Models/MemberShipType.cs
+++ Showie/Models/MemberShipType.cs
@@ -7,5 +7,9 @@ namespace Showie.Models
 {
     public class MemberShipType
     {
+        public short id { get; set; }
+        public int SignUpFee { get; set; }
+        public int DurationInMonths { get; set; }
+        public int DiscountInRate { get; set; }
     }
 }
