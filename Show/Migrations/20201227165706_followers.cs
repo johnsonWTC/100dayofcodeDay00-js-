4ceb533fd79b14394c33cda185b69532683fcffe
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-10 19:01:27 +0200

    init

---
+++ Show/Migrations/20201227165706_followers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Show.Migrations
+{
+    public partial class followers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
