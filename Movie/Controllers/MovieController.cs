6f9039a08d43caabc965831d7320f738ae6da5c0
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-13 20:50:48 +0200

    init

--- Movie/Controllers/MovieController.cs
+++ Movie/Controllers/MovieController.cs
@@ -22,7 +22,7 @@ namespace Movie.Controllers
             customers.Add(customer);
             customers.Add(customer1);
 
-          RandomMovieVieModel randomMovieVieModel
+          RandomMovieVieModel randomMovieVieModel = 
 
           
 
