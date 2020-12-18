bb7c2bc7120e4da9b1916f8fb46c9668293a8b14
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-18 20:33:14 +0200

    init

--- Movie/Controllers/MovieController.cs
+++ Movie/Controllers/MovieController.cs
@@ -15,7 +15,7 @@ namespace Movie.Controllers
             Customer customer = new Customer();
             customer.Name = "Johnson Dubula";
 
-            Customer customer1 = new Customer
+            Customer customer1 = new Customer()
 
 
             Show show = new Show();
