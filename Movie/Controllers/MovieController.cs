f2851504b9bb2e8b3fcf662f6e41b05aa7293399
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-19 20:28:34 +0200

    init

--- Movie/Controllers/MovieController.cs
+++ Movie/Controllers/MovieController.cs
@@ -13,7 +13,7 @@ namespace Movie.Controllers
         public IActionResult Random()
         {
             Customer customer = new Customer();
-            customer
+            customer.Name
             Show show = new Show();
             show.Name = "Mad Man";
             show.Id = 0;
