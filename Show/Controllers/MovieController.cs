3b4b98780ca298cb77fe4c0e3cb5cd505b7201c4
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-27 08:07:44 +0200

    API

--- Show/Controllers/MovieController.cs
+++ Show/Controllers/MovieController.cs
@@ -18,7 +18,7 @@ namespace Show.Controllers
         // GET: MovieController/Details/5
         public ActionResult Random(int id)
         {
-
+            var movie =
             return View();
         }
 
