b09e6503443b59c346c152948487c36824e146ee
Johnson Dubula <jdubula@iqbusiness.net>
2020-12-27 09:27:16 +0200

    API

--- Show/ViewModel/ViewModel.cs
+++ Show/ViewModel/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Show.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace Show.ViewModel
 {
     public class ViewModel
     {
-        public List<Movie> MyProperty { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
