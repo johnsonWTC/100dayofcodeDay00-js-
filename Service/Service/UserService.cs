34b6e41152504a0668221a501cc912b8ea9928d4
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-01 18:45:31 +0200

    init

--- Service/Service/UserService.cs
+++ Service/Service/UserService.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Service.Interface;
+using Service.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Service.Service
 {
-    public class UserService
+    public class UserService : UserI
     {
+        public 
+        public IEnumerable<User> getallUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
