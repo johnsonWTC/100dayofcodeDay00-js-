9e355a844cc03e02b975f36683a0c02c6c4c0aab
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-01 18:33:10 +0200

    init

--- Service/Startup.cs
+++ Service/Startup.cs
@@ -24,6 +24,11 @@ namespace Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Context>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
