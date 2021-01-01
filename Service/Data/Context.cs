14c6e9eec8eccf245e002dc8bdcfa9dd2e900006
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-01 18:28:34 +0200

    init

--- Service/Data/Context.cs
+++ Service/Data/Context.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Service.Data
 {
-    public class Context
+    public class Context :DbContext
     {
+        public SimpleImageGalleryDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<GalleryImage> GalleryImages { get; set; }
+    public DbSet<ImageTag> ImageTags { get; set; }
+}
+{
     }
 }
