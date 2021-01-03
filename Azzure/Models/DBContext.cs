d4646e334fc815edb74b0423292d7fa4b7b87716
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-04 02:00:00 +0300

    message

--- Azzure/Models/DBContext.cs
+++ Azzure/Models/DBContext.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using Azzure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Azzure.Models
 {
-    
-        public Context(DbContextOptions options) : base(options)
-        {
+    public class DBContext : DbContext
+    {
+    public DBContext(DbContextOptions options) : base(options)
+    {
 
-        }
-
-        public DbSe t<Image> Images { get; set; }
     }
+
+    public DbSet<Image> Images { get; set; }
+
+
+    }
+
+}
 
