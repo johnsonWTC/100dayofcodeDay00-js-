d7a9da9adabd44646087b38d447a04c8a20b084a
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-04 22:22:14 +0200

    contacts

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+    }
+}
