c20e3e10a4616c568eafe70b5107a26722573524
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-05 22:27:14 +0200

    photo model

---
+++ PhotoApp/Models/Photos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoApp.Models
+{
+    public class Photos
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
