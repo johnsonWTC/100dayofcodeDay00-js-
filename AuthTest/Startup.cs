4564e8108a2dcd54ad0e22702d515851dd13143c
Johnson Dubula <jdubula@iqbusiness.net>
2021-04-07 16:49:04 +0200

    Auth start up

--- AuthTest/Startup.cs
+++ AuthTest/Startup.cs
@@ -53,6 +53,7 @@ namespace AuthTest
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
