eb9e9c34b365502ab01c134b4c0c046a24d7da79
Johnson Dubula <jdubula@iqbusiness.net>
2021-04-16 19:28:15 +0200

    Working

---
+++ True/True/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace True.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
